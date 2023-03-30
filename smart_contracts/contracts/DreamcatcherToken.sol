531eaf54ccc9eb1b6e486ef7771adf55bd4aa131
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-03-30 23:48:14 +0100

    solved problem with import error, issue is in solidity compiler on vscode extension

--- smart_contracts/contracts/DreamcatcherToken.sol
+++ smart_contracts/contracts/DreamcatcherToken.sol
@@ -1,48 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+// install 0.0.135 vscode solidity extension because latest one doesnt work for imports
+import "smart_contracts\node_modules\@openzeppelin\contracts\token\ERC20\ERC20.sol";
 
-contract DreamcatcherToken is
-    Initializable,
-    ERC20Upgradeable,
-    ERC20BurnableUpgradeable,
-    PausableUpgradeable,
-    OwnableUpgradeable
-{
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize() public initializer {
-        __ERC20_init("Dreamcatcher", "DREAM");
-        __ERC20Burnable_init();
-        __Pausable_init();
-        __Ownable_init();
-    }
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
-    }
+contract DreamcatcherToken {
+    
 }
