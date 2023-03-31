425e68b3efac21de4909928eeb5f27a0c0c402a6
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-03-31 14:57:52 +0100

    Update DreamcatcherToken.sol

--- smart_contracts/contracts/DreamcatcherToken.sol
+++ smart_contracts/contracts/DreamcatcherToken.sol
@@ -1,23 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+/*
+The token max supply is uncapped to allow the governor to raise funds
+However, it will require proposals and heavy voting to be able to do such a thing
+As holders would not be incetizied to do so, unless the proposal was good
+If the DAO is making money then they can also burn tokens too
+*/
 
 // snapshot governance voting compatibility
 // install 0.0.135 vscode solidity extension because latest one doesnt work for imports
 import "smart_contracts\node_modules\@openzeppelin\contracts\token\ERC20\ERC20.sol";
 import "smart_contracts\node_modules\@openzeppelin\contracts\token\ERC20\extensions\ERC20Burnable.sol";
 import "smart_contracts\node_modules\@openzeppelin\contracts\access\AccessControl.sol";
-import "smart_contracts\node_modules\@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "smart_contracts\node_modules\@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Votes.sol";
-import "smart_contracts\node_modules\@openzeppelin/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
+import "smart_contracts\node_modules\@openzeppelin\contracts\token\ERC20\extensions\draft-ERC20Permit.sol";
+import "smart_contracts\node_modules\@openzeppelin\contracts\token\ERC20\extensions\draft-ERC20Votes.sol";
+//
+import "smart_contracts\node_modules\@openzeppelin\contracts\token\ERC20\extensions\TokenSnapshot.sol";
+
 contract DreamcatcherToken is 
     ERC20, 
-    ERC20Burnable, 
+    ERC20Burnable,
+    ERC20Snapshot, 
     AccessControl, 
     ERC20Permit, 
-    ERC20Votes,
-    ERC20VotesComp {
-        // management and roles
-        address public governor; // governor contract
+    ERC20Votes {
+        // define the governor address
+        address public governor;
         address public team =msg.sender;
 
         // token details
@@ -27,7 +35,7 @@ contract DreamcatcherToken is
         string public totalSupply =initialSupply;
 
         constructor()
-            ERC20(name, symbol) 
+            ERC20(name, symbol)
             ERC20Permit(name) {
                 _mint(governor, initialSupply * 10 ** decimals());
         }
