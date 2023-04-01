171be70df5ba23a4939ccf167f189da74d9317eb
snqre <marco.bizzaro@gmail.com>
2023-04-01 22:34:57 +0100

    Dreamcatcher updates

--- smart_contracts/contracts/DreamcatcherToken.sol
+++ smart_contracts/contracts/DreamcatcherToken.sol
@@ -9,22 +9,50 @@ If the DAO is making money then they can also burn tokens too
 
 // snapshot governance voting compatibility
 // install 0.0.135 vscode solidity extension because latest one doesnt work for imports
-import "smart_contracts\node_modules@openzeppelincontracts\tokenERC20ERC20.sol";
-import "smart_contracts\node_modules\@openzeppelin\contracts\access\Ownable.sol";
+import "smart_contracts/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "smart_contracts/node_modules/@openzeppelin/contracts/access/AccessControl.sol";
+import "smart_contracts/node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract DreamcatcherToken is ERC20, Ownable {
-    // define the governor address
-    // tokens will be trasnfered here first then the governor will have init logic for the team
-    address public addressGovernor;
+contract Dreamcatcher is ERC20, AccessControl {
+    // DIFFERENT TYPES OF ROLES
+    // only the custodian can mint tokens 
+    bytes32 public constant CUSTODIAN = keccak256("CUSTODIAN"); // this is the contract itself responsible for maintaining and assiging roles, the custodian only can mint and control key features
+    bytes32 public constant DIRECTOR = keccak256("DIRECTOR"); // ideally head to allow the project to move in a particular direction
+    bytes32 public constant BOARD_MEMBER = keccak256("BOARD_MEMBER"); // the board members are elected every x amount of time, these are 6 - 16 people elected and only they can propose actions and stuff that can be voted on
+    bytes32 public constant MEMBER = keccak256("MEMBER"); // token holders only they can actively vote and access certain functions
 
-    // token details
+    address public addressCustodian = msg.sender;
     string public stringName = "Dreamcatcher";
     string public stringSymbol = "DREAM";
     uint256 public uint256InitialSupply = 100000;
 
+    function giveCustodianRole(address _addressCustodian) private {
+        /* give honorary custodian role to an impartial party set by code */
+        _setupRole(DEFAULT_ADMIN_ROLE, _addressCustodian);
+        _setupRole(CUSTODIAN, _addressCustodian);
+    }
+
     constructor() ERC20(stringName, stringSymbol) {
-        transferOwnership(addressGovernor);
-        _mint(addressGovernor, uint256InitialSupply * 10 **decimals());
+        // give the contract custodian role
+        giveCustodianRole(addressCustodian);
+
+        // generate initial supply for the custodian
+        mint(uint256InitialSupply);
+
+    }
+
+    function mint_for(address to, uint256 amount) public {
+        /* only the custodian can mint tokens for others */
+        /* also has decimals automatically calculated */
+        require(hasRole(CUSTODIAN, msg.sender), "Only the Custodian can mint tokens for others:: must go through proposal");
+        _mint(to, amount * 10 **decimals());
+    }
+
+    function mint(uint256 amount) public {
+        /* only the custodian can mint tokens for itself */
+        /* also has decimals automatically calculated */
+        require(hasRole(CUSTODIAN, msg.sender), "Only the Custodian can mint tokens for itself:: must go through proposal");
+        _mint(addressCustodian, amount * 10 **decimals());
     }
 
 }
