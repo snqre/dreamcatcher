1f0d2e6b0b69bc05e1677c9bcd7ab0fcd3c02d21
snqre <marco.bizzaro@gmail.com>
2023-04-02 00:36:55 +0100

    Dreamcatcher updates

--- smart_contracts/contracts/DreamcatcherToken.sol
+++ smart_contracts/contracts/DreamcatcherToken.sol
@@ -12,13 +12,13 @@ If the DAO is making money then they can also burn tokens too
 import "smart_contracts/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "smart_contracts/node_modules/@openzeppelin/contracts/access/AccessControl.sol";
 import "smart_contracts/node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "smart_contracts\node_modules@openzeppelincontracts\tokenERC20extensionsERC20Votes.sol";
-import "smart_contracts\node_modules@openzeppelincontracts\tokenERC20extensionsERC20Snapshot.sol";
-import "smart_contracts\node_modules@openzeppelincontracts\tokenERC20IERC20.sol";
-import "smart_contracts\node_modules@openzeppelincontracts\tokenERC20\utilsSafeERC20.sol";
-import "smart_contracts\node_modules@openzeppelincontracts\utilsmathSafeMath.sol";
+import "smart_contracts/node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "smart_contracts/node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "smart_contracts/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "smart_contracts/node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "smart_contracts/node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Dreamcatcher is ERC20, ERC20Votes, ERC20Snapshot, AccessControl {
+contract Dreamcatcher is ERC20, AccessControl {
     // DIFFERENT TYPES OF ROLES
     // only the custodian can mint tokens
     bytes32 public constant CUSTODIAN = keccak256("CUSTODIAN"); // this is the contract itself responsible for maintaining and assiging roles, the custodian only can mint and control key features
@@ -45,7 +45,6 @@ contract Dreamcatcher is ERC20, ERC20Votes, ERC20Snapshot, AccessControl {
 
         // generate initial supply for the custodian
         mint(uint256InitialSupply);
-        snapshot(); // call after minting
     }
 
     function mint_for(address to, uint256 amount) public {
@@ -56,7 +55,6 @@ contract Dreamcatcher is ERC20, ERC20Votes, ERC20Snapshot, AccessControl {
             "Only the Custodian can mint tokens for others:: must go through proposal"
         );
         _mint(to, amount * 10**decimals());
-        snapshot(); // call after minting
     }
 
     function mint(uint256 amount) public {
@@ -67,82 +65,6 @@ contract Dreamcatcher is ERC20, ERC20Votes, ERC20Snapshot, AccessControl {
             "Only the Custodian can mint tokens for itself:: must go through proposal"
         );
         _mint(addressCustodian, amount * 10**decimals());
-        snapshot();
     }
 
-    function getLastSnapshotId() public view returns (uint256) {
-        return latestSnapshot();
-    }
-
-    function balanceOfAt(address account, uint256 snapshotId)
-        public
-        view
-        returns (uint256)
-    {
-        return balanceOfAt(account, snapshotId);
-    }
-
-    function totalSupplyAt(uint256 snapshotId) public view returns (uint256) {
-        return totalSupplyAt(snapshotId);
-    }
-
-    // TREASURY STUFF DRAFT
-    function isTokenSupported(
-        address addressToken //checks what types of tokens are valid for the treasury
-    )
-        internal
-        view
-        returns (
-            /* check if we have more than 0 of the token */
-            bool
-        )
-    {
-        // check if the token has a balance greater than zero in the cotnract
-        uint256 uint256Balance = IERC20(addressToken).balanceOf(address(this));
-        if (uint256Balance == 0) {
-            //if we dont have any, we dont support it
-            return false;
-        }
-
-        // if the token passed both checks, it is supported
-        return true;
-    }
-
-    function getSupportedTokens(address _addressTokenAddress)
-        external
-        view
-        returns (
-            /* find all non zero balances useful so we can calculate the value of our treasury vs assets*/
-            address[] memory
-        )
-    {
-        IERC20 IERC20Token = IERC20(_addressTokenAddress);
-        uint256 uint256TokenCount = IERC20Token.balanceOf(address(this));
-        address[] memory addressArrSupportedTokens = new address[](
-            uint256TokenCount
-        );
-        uint256 uint256SupportedTokenIndex = 0;
-
-        for (uint256 i = 0; i < uint256TokenCount; i++) {
-            address addressCurrentToken = IERC20Token.tokenOfOwnerByIndex(
-                address(this),
-                i
-            );
-            if (isTokenSupported(addressCurrentToken)) {
-                addressArrSupportedTokens[
-                    uint256SupportedTokenIndex
-                ] = addressCurrentToken;
-                uint256SupportedTokenIndex++;
-            }
-        }
-
-        address[] memory addressArrFinalSupportedTokens = new address[](
-            uint256SupportedTokenIndex
-        );
-        for (uint256 i = 0; i < uint256SupportedTokenIndex; i++) {
-            addressArrFinalSupportedTokens[i] = addressArrSupportedTokens[i];
-        }
-
-        return addressArrFinalSupportedTokens; //return array with all the non zero balance tokens we have in treasury including ours
-    }
 }
