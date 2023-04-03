3421010179bf4413070199e862494df6cc110647
snqre <marco.bizzaro@gmail.com>
2023-04-03 11:23:57 +0100

    dream

--- smart_contracts/contracts/DreamcatcherToken.sol
+++ smart_contracts/contracts/DreamcatcherToken.sol
@@ -50,6 +50,41 @@ contract Dreamcatcher {
         mapping(address => bool) isMember;
     }
 
+    struct Custodian {
+        /* these can all be changed with a proposal -- future proofing the contract */
+        uint256 maxBoardMembers;
+        uint256 minBoardMembers;
+        uint256 minDirectors;
+        uint256 maxDirectors;
+        uint256 minMember;
+        uint256 maxMember;
+
+        uint256 minBalanceToBeBoardMember;
+        uint256 maxBalanceToBeBoardMember;
+        uint256 minVestedToBeBoardMember;
+        uint256 maxVestedToBeBoardMember;
+        uint256 minStakedToBeBoardMember;
+        uint256 maxStakedToBeBoardMember;
+        uint256 minVotesToBeBoardMember;
+
+        uint256 minBalanceToBe
+
+
+
+        /* proposals */
+        uint256 percentTotalSupplyToPass;
+
+
+
+        /* services */
+        uint256 percentEarningsDistributed;//how much is 
+        uint256 percentEarnignsDistributedThroughBurning;
+
+
+        bool isPausing;
+
+    }
+    // -- MAIN
     Token public dreamcatcherToken;
 
     constructor() {
@@ -63,44 +98,90 @@ contract Dreamcatcher {
         dreamcatcherToken.isBurnable = true;
         dreamcatcherToken.isTransferable = true;
 
+        //by default the launched contract address will be custodian
+        dreamcatcherToken.isCustodian[msg.sender] = true;
+
         //instructions on deploy
         //send 20% to team wallet
         //send x% for private funding
         //keep x amount on contract as the contract will be the one selling directly
     }
 
-    modifier onlyCustodian() {//note im not sure if all account roles start with false yet
+    // -- FUNCTIONALITY AND FEATURES
+    /* events */
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 amount
+    );
+
+    modifier onlyCustodian() {
+        //note im not sure if all account roles start with false yet
         /* check if address is custodian */
         require(
-            dreamcatcherToken.isCustodian[msg.sender],
-            "Only the Custodian can do this. You must go through a proposal to do this."
+            dreamcatcherToken.isCustodian[msg.sender] == true,
+            "Only the Custodian can access this. Use a proposal or vote on it. Elect a board member"
         );
         _;
     }
 
-    function name() public view virtual override returns (string memory) {
+    modifier onlyFounder() {
+        /* check if address is founder */
+        require(
+            dreamcatcherToken.isFounder[msg.sender] == true,
+            "Only the Founder can access this. Contact the founder"
+        );
+        _;
+    }
+
+    modifier onlyBoardMember() {
+        //note im not sure if all account roles start with false yet
+        /* check if address is custodian */
+        require(
+            dreamcatcherToken.isBoardMember[msg.sender] == true,
+            "Only a board member can access this. vote for a board member or campaign to become one"
+        );
+        _;
+    }
+
+    modifier onlyDirector() {
+        //note im not sure if all account roles start with false yet
+        /* check if address is custodian */
+        require(
+            dreamcatcherToken.isDirector[msg.sender] == true,
+            "Only the Custodian can access this. Use a proposal or vote on it. Elect a board member"
+        );
+        _;
+    }
+
+    modifier onlyMember() {
+        //note im not sure if all account roles start with false yet
+        /* check if address is custodian */
+        require(
+            dreamcatcherToken.isMember[msg.sender] == true,
+            "You must wait to become a voting member of this community"
+        );
+        _;
+    }
+
+    function name() public view virtual returns (string memory) {
         return dreamcatcherToken.name;
     }
 
-    function symbol() public view virtual override returns (string memory) {
+    function symbol() public view virtual returns (string memory) {
         return dreamcatcherToken.symbol;
     }
 
-    function decimals() public view virtual override returns (uint8) {
+    function decimals() public view virtual returns (uint8) {
         return dreamcatcherToken.decimals;
     }
 
-    function totalSupply() public view virtual override returns (uint256) {
+    function totalSupply() public view virtual returns (uint256) {
         return dreamcatcherToken.totalSupply;
     }
 
-    function balanceOf(address account)
-        public
-        view
-        virtual
-        override
-        returns (uint256)
-    {
+    function balanceOf(address account) public view virtual returns (uint256) {
         /* return how much the address has of our token */
         return dreamcatcherToken.balance[account];
     }
@@ -109,167 +190,111 @@ contract Dreamcatcher {
         public
         view
         virtual
-        override
         returns (uint256)
     {
         /* return the amount of tokens of the account which are locked */
         return dreamcatcherToken.vested[account];
     }
 
-    function balanceStaked(address account) public view virtual override returns (uint256) {
+    function balanceStaked(address account)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
         /* return the amount of tokens which are staked */
         return dreamcatcherToken.staked[account];
     }
 
-    function votes(address account) public view virtual override returns (uint256) {
+    function votes(address account) public view virtual returns (uint256) {
         /* return number of votes the person has */
         return dreamcatcherToken.votes[account];
     }
 
-    // transfer, transferfrom, approve, allowance
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // CUSTODIAN DATA
-    struct Custodian {
-        address account;
-        // state machine
-        bool is_polling;
-    }
-
-    Custodian custodian = Custodian("", msg.sender);
-
-    // DIFFERENT TYPES OF ROLES
-    // only the custodian can mint tokens
-    bytes32 public constant CUSTODIAN = keccak256("CUSTODIAN"); // this is the contract itself responsible for maintaining and assiging roles, the custodian only can mint and control key features
-    bytes32 public constant DIRECTOR = keccak256("DIRECTOR"); // ideally head to allow the project to move in a particular direction
-    bytes32 public constant BOARD_MEMBER = keccak256("BOARD_MEMBER"); // the board members are elected every x amount of time, these are 6 - 16 people elected and only they can propose actions and stuff that can be voted on
-    bytes32 public constant MEMBER = keccak256("MEMBER"); // token holders only they can actively vote and access certain functions
-
-    address public addressCustodian = msg.sender;
-    string public stringName = "Dreamcatcher";
-    string public stringSymbol = "DREAM";
-    uint256 public uint256InitialSupply = 100000;
-
-    /*
-        a project has a team behind it
-        payment is split up into period of time to avoid malicious behaviour
-        if the board sees that the team is getting things done they will keep paying
-        else they can stop
-    */
-    struct Project {
-        string name;
-        string description;
-        address funding;
-        uint256 duration;
-        uint256 budget;
-    }
-
-    /* THIS IS IMPORTANT STUFF THAT CAN BE CHANGED ONLY BY PROPOSAL */
-    function giveCustodianRole(address _addressCustodian) private {
-        /* give honorary custodian role to an impartial party set by code */
-        _setupRole(DEFAULT_ADMIN_ROLE, _addressCustodian);
-        _setupRole(CUSTODIAN, _addressCustodian);
-    }
-
-    constructor() ERC20(stringName, stringSymbol) {
-        // give the contract custodian role
-        giveCustodianRole(addressCustodian);
-
-        // generate initial supply for the custodian
-        mint(dreamcatcherToken.initialSupply);
-    }
-
-    modifier onlyCustodian() {
-        require(
-            hasRole(CUSTODIAN, msg.sender),
-            "Only the Custodian can do this. You must go through a proposal to do this."
-        );
-        _;
-    }
-
-    // SNAPSHOT
-
-    // MINTING
-    function mintFor(address addressAccount, uint256 uint256Amount)
+    function allowance(address owner, address spender)
         public
-        onlyCustodian
+        view
+        returns (uint256)
     {
-        /* only the custodian can mint tokens for others */
-        /* also has decimals automatically calculated */
-        _mint(addressAccount, uint256Amount * 10**decimals());
+        return dreamcatcherToken.allowance[owner][spender];
     }
 
-    function mint(uint256 uint256Amount) public onlyCustodian {
-        /* only the custodian can mint tokens for itself */
-        /* also has decimals automatically calculated */
-        _mint(addressCustodian, uint256Amount * 10**decimals());
-    }
-
-    // BURNING
-    function burn(uint256 amount) public virtual onlyCustodian {
-        /* only the custodian can burn tokens for itself, people can send tokens here if they want to burn them */
-        _burn(addressCustodian, amount);
-    }
-
-    // PAUSING -- the community can vote to emergency pause if neccesary
-
-    // START PROJECT
-    function beginProject() public onlyCustodian {
-        Project project = Project(
-            "Baking Beans",
-            "I want to bake beans",
-            msg.sender,
-            365,
-            2000
+    // transfer, transferfrom, approve, allowance
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(
+            dreamcatcherToken.balance[msg.sender] >= amount,
+            "Insufficient balance"
         );
-        project.name;
-        project.description;
-        project.funding;
-
-        // VOTING LOGIC
-
-        // IF VOTE PASS THEN DO
-        // send initial moola to team address
-        // emit message saying this has happened
+        dreamcatcherToken.balance[msg.sender] -= amount;
+        dreamcatcherToken.balance[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
     }
 
-    // RAISE FUNDING
+    function transferfrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public returns (bool) {
+        require(
+            dreamcatcherToken.balance[sender] >= amount,
+            "Insufficient balance"
+        );
+        require(
+            dreamcatcherToken.allowance[sender][msg.sender] >= amount,
+            "Transfer amount exceeds allowance"
+        );
+
+        dreamcatcherToken.balance[sender] -= amount;
+        dreamcatcherToken.balance[recipient] += amount;
+        dreamcatcherToken.allowance[sender][msg.sender] -= amount;
+
+        emit Transfer(sender, recipient, amount);
+
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        dreamcatcherToken.allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
     /*
-    custodian can mint new tokens and these can be used to raise funding at a certain price per eth or crypto
-    as this can devalue other people's holdings this must be voted on
-    to avoid attacks and malicious behaviour people must not be able to just buy 60% of the supply and do this
+        all roles can be granted except custodian and founder
     */
-    function raiseCapital(uint256 amount) public onlyCustodian {
-        mint(amount);
+    function grantIsBoardMember(address account) public onlyCustodian {
+        /* grant board member role */
+        dreamcatcherToken.isBoardMember[account] = true;
     }
 
-    // BUDGET
-    /* a budget is an amount of money or crypto that will be dedicated to an address or cause 
-        this can be sent on a daily/weekly, or whatever timeframe and does not have to be used up
-        in case of malicious behaviour if the entity is thought as not doing the job payments to the account will stop
-        payments to the account will also stop if the budget is finished and no budget has been allocated
-        unlike the treasury, the budget is set by members and receiving address must also be voted on
-    */
+    function revokeIsBoardMember(address account) public onlyCustodian {
+        /* revoke board member role */
+        dreamcatcherToken.isBoardMember[account] = false;
+    }
 
-    function budget(uint256 amount, string project) onlyCustodian {
-        // this amount of tokens will
+    function grantIsDirector(address account) public onlyCustodian {
+        /* grant director role */
+        dreamcatcherToken.isDirector[account] = true;
+    }
+
+    function revokeIsDirector(address account) public onlyCustodian {
+        /* revoke director role */
+        dreamcatcherToken.isDirector[account] = false;
+    }
+
+    /*
+        membership is important and sets who can take part in voting
+        we don't want people who just bought their tokens 2 days ago to try and vote
+        we want dedicated people to be responsible no moon boiis
+    */
+    function grantIsMember(address account) public onlyCustodian {
+        /* grant membership */
+        dreamcatcherToken.isMember[account] = true;
+    }
+
+    function revokeIsMember(address account) public onlyCustodian {
+        /* revoke membership */
+        dreamcatcherToken.isMember[account] = false;
     }
 }
