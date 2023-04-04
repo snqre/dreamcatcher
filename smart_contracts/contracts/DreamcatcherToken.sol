2f605cca51665473146d717df8d4f82404d17da2
snqre <marco.bizzaro@gmail.com>
2023-04-04 17:02:50 +0100

    updates

--- smart_contracts/contracts/DreamcatcherToken.sol
+++ smart_contracts/contracts/DreamcatcherToken.sol
@@ -16,6 +16,45 @@ If the DAO is making money then they can also burn tokens too
     mint
 
 */
+
+import "smart_contracts/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract TimeLock {
+    /*
+        responsible for vesting
+    */
+    uint256 public constant duration = 365 days;
+    uint256 public immutable end;
+    address payable public immutable owner;
+ 
+    constructor(address payable owner) {
+        end = block.timestamp + duration;
+        owner = owner;
+    }
+
+    function deposit(address token, uint256 amount) external {
+        /* 
+            deposit the amount of tokens to this contract
+            param 
+        
+        */
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+    }
+
+    receive() external payable {}
+
+    function withdraw(address token, uint256 amount) external {
+        require(msg.sender == owner, "only owner");
+        require(block.timestamp >= end, "too early");
+        if (token == address(0)) {
+            owner.transfer(amount);
+        } else {
+            IERC20(token).transfer(owner, amount);
+        }
+    }
+}
+
 contract DreamcatcherToken {
     /* =.=.=.=.=.=.=.= TOKEN =.=.=.=.=.=.=.= */
     struct TokenIERC20 {
@@ -93,6 +132,16 @@ contract DreamcatcherToken {
         Syndicate syndicate;
         Vault vault;
     }
+
+    /* =.=.=.=.=.=.=.= VESTING =.=.=.=.=.=.=.= */
+    struct VestingSchedule {
+        uint256 amount;
+        uint256 start;
+        uint256 duration;
+        uint256 claimed;
+    }
+
+    mapping(address => VestingSchedule[]) public vestingSchedules;
 
     /* =.=.=.=.=.=.=.= INIT =.=.=.=.=.=.=.= */
     Token private dreamcatcherToken;
@@ -204,6 +253,13 @@ contract DreamcatcherToken {
         dreamcatcherToken.IERC20.totalSupply += amount;
         emit Mint(account, amount);
     }
+
+    function mintVested(
+        address account,
+        uint256 amount,
+        uint256 start,
+        uint256 duration
+    ) public onlyCustodian {}
 
     function burn(address account, uint256 amount) public onlyCustodian {
         dreamcatcherToken.balance[account] -= amount;
