3f4cb16a925145c38fca0515bbb28350ff3afc07
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-04-04 19:30:31 +0100

    Update DreamcatcherToken.sol

--- smart_contracts/contracts/DreamcatcherToken.sol
+++ smart_contracts/contracts/DreamcatcherToken.sol
@@ -17,44 +17,6 @@ If the DAO is making money then they can also burn tokens too
 
 */
 
-import "smart_contracts/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-
-contract TimeLock {
-    /*
-        responsible for vesting
-    */
-    uint256 public constant duration = 365 days;
-    uint256 public immutable end;
-    address payable public immutable owner;
- 
-    constructor(address payable owner) {
-        end = block.timestamp + duration;
-        owner = owner;
-    }
-
-    function deposit(address token, uint256 amount) external {
-        /* 
-            deposit the amount of tokens to this contract
-            param 
-        
-        */
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
-    }
-
-    receive() external payable {}
-
-    function withdraw(address token, uint256 amount) external {
-        require(msg.sender == owner, "only owner");
-        require(block.timestamp >= end, "too early");
-        if (token == address(0)) {
-            owner.transfer(amount);
-        } else {
-            IERC20(token).transfer(owner, amount);
-        }
-    }
-}
-
 contract DreamcatcherToken {
     /* =.=.=.=.=.=.=.= TOKEN =.=.=.=.=.=.=.= */
     struct TokenIERC20 {
