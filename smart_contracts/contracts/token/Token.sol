e4a72547d740b73b89ce73bfb44555121eeec706
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-04-15 22:21:55 +0100

    Update Token.sol

--- smart_contracts/contracts/token/Token.sol
+++ smart_contracts/contracts/token/Token.sol
@@ -7,6 +7,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
+
 contract Token is ERC20, ERC20Votes, ERC20Snapshot, ERC20Permit, ERC20Capped {
     constructor() ERC20("Dreamcatcher", "DREAM") ERC20Capped(200000000 * 10**decimals())
 }
