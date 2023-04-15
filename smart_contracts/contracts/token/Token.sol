953c483527f3b866b4d686b9e7726b6e1a0584b3
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-04-15 23:14:36 +0100

    Update Token.sol

--- smart_contracts/contracts/token/Token.sol
+++ smart_contracts/contracts/token/Token.sol
@@ -1,17 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Capped.sol";
+contract Token {
+    constructor() {}
 
-
-contract Token is ERC20, ERC20Permit, ERC20Votes, ERC20Snapshot, ERC20Capped {
-    constructor() ERC20("Dreamcatcher", "DREAM") ERC20Capped(200000000 * 10**decimals()) {}
-
-    function _mint(address _owner, uint256 _amount) internal override {
-        super._mint(_owner, _amount);
-    }
 }
