19a2e10ff6378f9ad8f610366e7c7d8655d18f1e
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-04-15 22:21:05 +0100

    Update Token.sol

--- smart_contracts/contracts/token/Token.sol
+++ smart_contracts/contracts/token/Token.sol
@@ -1,23 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "dreamcatcher/.deps/openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "dreamcatcher/.deps/openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "dreamcatcher/.deps/openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
-import "dreamcatcher/.deps/openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "dreamcatcher/.deps/openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-    ERC20,
-    ERC20Votes,
-    ERC20Snapshot,
-    ERC20Permit,
-    ERC20Capped
-{
-    /**
-    200 million total supply && max supply
-     */
-    constructor()
-        ERC20("Dreamcatcher", "DREAM")
-        ERC20Capped(200000000 * 10**decimals())
-    {}
+contract Token is ERC20, ERC20Votes, ERC20Snapshot, ERC20Permit, ERC20Capped {
+    constructor() ERC20("Dreamcatcher", "DREAM") ERC20Capped(200000000 * 10**decimals())
 }
