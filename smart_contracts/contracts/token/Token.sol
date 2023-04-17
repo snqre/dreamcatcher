90c4d13c91c13898301a449ad6ca6b24e49325ab
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-04-17 01:06:56 +0100

    Update Token.sol

--- smart_contracts/contracts/token/Token.sol
+++ smart_contracts/contracts/token/Token.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// RESTARTED, I THINK I CAN DO IT BETTER
-
-
+/**
+* @title Authenticator
+* @todo 
+*
+*
+*
+*
+*
+ */
+contract Authenticator {
+    bool locked;
+    modifier mutex() {
+        require(!locked, "Mutex: Reentrant call");
+        assembly {sstore(locked_slot, 1)}
+        locked = true;
+        _;
+        locked = false;
+    }
+}
