b19926030b4219d0b5d8d92bb1d392787da6ddb5
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-04-23 20:07:34 +0100

    Update Token.sol

--- smart_contracts/contracts/Token/Token.sol
+++ smart_contracts/contracts/Token/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "./smart_contracts/contracts/Token/Authenticator.sol";
 
 interface IERC20 {
-
+    // =.=.=.=.= PUBLIC
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
     function decimals() external view returns (uint8);
@@ -14,13 +14,13 @@ interface IERC20 {
     function allowance(address _owner, address _spender) external view returns (uint256 remaining);
     function approve(address _spender, uint256 _value) external returns (bool success);
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
-
+    // =.=.=.=.= EVENTS
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
 interface ICustomToken {
-
+    // =.=.=.=.= PUBLIC
     function stake(uint256 _value) external returns (bool);
     function unstake(uint256 _value) external returns (bool);
     function release(string memory _caption) external returns (bool);
@@ -31,30 +31,46 @@ interface ICustomToken {
     function mintable() external view returns (uint256);
     function stakeOf(address _owner) external view returns (uint256);
     function votesOf(address _owner) external view returns (uint256);
+    // =.=.=.=.= ADMIN ONLY
+    function mint(address _to, uint256 _value) external returns (bool);
+    function mintWithVesting(address _to, uint256 _value, uint256 _duration, string memory _caption) returns (bool);
+    function burn(uint256 _value) external returns (bool);
+    function fetchSettings() external returns (
+        uint256,
+        uint256,
+        uint256,
+        uint256,
+        uint256,
+        uint256
+    );
+    function updateSettings(uint256 _bpFeeBurn, uint256 _bpFeeBank) external returns (bool);
+    function update(address _vault) external returns (bool);
+    // =.=.=.=.= EVENTS
+    event UpdateToSettings(uint256 _bpFeeBurn, uint256 _bpFeeBank);
+    event Update(address indexed _vault);
 }
 
 contract Token is Authenticator, IERC20, ICustomToken {
 
-    constructor(address _vault) {
-        
+    constructor(address ) {
+        address _contract = msg.sender;
+        address _dev = 0xDbF85074764156004FEb245b65693e59a62262c2;
+        grantPermissionAdmin(_contract);
+        grantPermissionAdmin(_dev);
+
         meta.name = "Dreamcatcher";                     // set name
         meta.symbol = "DREAM";                          // set symbol
         meta.decimals = 18;                             // set decimals
         meta.totalSupply = 0;                           // initial totalSupply
         meta.maxSupply = 200000000 * 10**meta.decimals; // 200000000.000000000000000000
         meta.mintable = meta.maxSupply;
-        meta.vault = _vault;                            // set vault address to contract address
-        settings.bpTransferBurn = 0;                    // 0 | 100 == 1%
-        settings.bpTransferBank = 0;                    // 0 | 100 == 1%
+        meta.vault = _contract;
+        settings.bpTransferBurn = 0;
+        settings.bpTransferBank = 0;
         settings.bpTransferBurnMin = 0;
         settings.bpTransferBankMin = 0;
         settings.bpTransferBurnMax = 10000;
         settings.bpTransferBankMax = 10000;
-
-        address _contract = msg.sender;
-        address _dev = 0xDbF85074764156004FEb245b65693e59a62262c2;
-        grantPermissionAdmin(_contract);
-        grantPermissionAdmin(_dev);
 
         mint_(meta.vault, 200000000 * 10**meta.decimals);
     }
@@ -186,10 +202,10 @@ contract Token is Authenticator, IERC20, ICustomToken {
             _value <= staked[_to]
         );
         (bool _success, uint256 _newValue) = transfer_(
-            _from, 
-            _to, 
-            _value, 
-            _bpFeeBurn, 
+            _from,
+            _to,
+            _value,
+            _bpFeeBurn,
             _bpFeeBank
         );
         staked[_to]      -= _value;
@@ -232,7 +248,20 @@ contract Token is Authenticator, IERC20, ICustomToken {
         return true;
     }
 
-    function fetchSettings() external onlyAdmin returns (uint256, uint256, uint256, uint256) {
+    function mint(address _to, uint256 _value) external onlyAdmin returns (bool) {
+        mint_(_to, _value);
+    }
+
+    function mintWithVesting(address _to, uint256 _value, uint256 _duration, string memory _caption) external onlyAdmin returns (bool) {
+        mintWithVesting_(_to, _value, _duration, _caption);
+    }
+
+    function burn(uint256 _value) external onlyAdmin returns (bool) {
+        address _from = msg.sender;
+        burn_(_from, _value);
+    }
+
+    function fetchSettings() external onlyAdmin returns (uint256, uint256, uint256, uint256, uint256, uint256) {
         uint256 _bpFeeBurn = settings.bpTransferBurn;
         uint256 _bpFeeBank = settings.bpTransferBank;
         uint256 _bpFeeBurnMin = settings.bpTransferBurnMin;
@@ -263,6 +292,7 @@ contract Token is Authenticator, IERC20, ICustomToken {
         );
         settings.bpTransferBurn = _bpFeeBurn;
         settings.bpTransferBank = _bpFeeBank;
+        emit UpdateToSettings(_bpFeeBurn, _bpFeeBank);
         return true;
     }
 
@@ -271,54 +301,20 @@ contract Token is Authenticator, IERC20, ICustomToken {
             _vault != address(0)
         );
         meta.vault = _vault;
+        emit Update(_vault);
         return true;
     }
 
-    function name() external view returns (string memory) {
-        return meta.name;
-    }
-
-    function symbol() external view returns (string memory) {
-        return meta.symbol;
-    }
-
-    function decimals() external view returns (uint8) {
-        return meta.decimals;
-    }
-
-    function totalVotes() external view returns (uint256) {
-        return meta.totalVotes;
-    }
-
-    function totalStaked() external view returns (uint256) {
-        return meta.totalStaked;
-    }
-
-    function totalSupply() external view returns (uint256) {
-        return meta.totalSupply;
-    }
-
-    function maxSupply() external view returns (uint256) {
-        return meta.maxSupply;
-    }
-
-    function mintable() external view returns (uint256) {
-        return meta.mintable;
-    }
-
-    function balanceOf(address _owner) external view returns (uint256) {
-        return balance[_owner];
-    }
-
-    function stakeOf(address _owner) external view returns (uint256) {
-        return staked[_owner];
-    }
-
-    function votesOf(address _owner) external view returns (uint256) {
-        return votes[_owner];
-    }
-
-    function allowance(address _owner, address _spender) external view returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
+    function name() external view returns (string memory) {return meta.name;}
+    function symbol() external view returns (string memory) {return meta.symbol;}
+    function decimals() external view returns (uint8) {return meta.decimals;}
+    function totalVotes() external view returns (uint256) {return meta.totalVotes;}
+    function totalStaked() external view returns (uint256) {return meta.totalStaked;}
+    function totalSupply() external view returns (uint256) {return meta.totalSupply;}
+    function maxSupply() external view returns (uint256) {return meta.maxSupply;}
+    function mintable() external view returns (uint256) {return meta.mintable;}
+    function balanceOf(address _owner) external view returns (uint256) {return balance[_owner];}
+    function stakeOf(address _owner) external view returns (uint256) {return staked[_owner];}
+    function votesOf(address _owner) external view returns (uint256) {return votes[_owner];}
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining) {return allowed[_owner][_spender];}
 }
