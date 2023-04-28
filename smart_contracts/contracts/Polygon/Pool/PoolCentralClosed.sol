65380434b36aa24a47f26166f171e5902993843e
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-04-28 15:00:42 +0100

    overhaul

---
+++ smart_contracts/contracts/Polygon/Pool/PoolCentralClosed.sol
@@ -0,0 +1,416 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+interface IERC20 {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+/** linked pool and closed centralized */
+contract State is IERC20 {
+    /** ERC20 */
+    struct Token {
+        string name;
+        string symbol;
+        uint8 decimals;
+        uint256 mintable;
+        uint256 totalSupply;
+    }
+    Token internal token;
+    mapping(address=>uint256) internal balance;
+    mapping(address=>uint256) internal allowed;
+    /** pool */
+        struct Fee {
+        uint256 management;
+        uint256[] performance;
+        uint256[] thresholds;
+        uint256 measurementStart;
+        uint256 measurementReset;
+    }
+    struct Settings {
+        bool externalTransferable;
+        bool onlyWhitelisted;
+    }
+    struct Funds {
+        uint256 minDeposit;
+        uint256 maxDeposit;
+    }
+    struct Pool {
+        string name;
+        string description;
+        uint256 required;
+        uint256 aum;
+        uint256 lbt;
+        uint256 nav;
+        uint256 navps;
+        Fee fee;
+        address creator;
+        uint256 creatorStake;
+        Settings settings;
+    }
+    Pool internal pool;
+    /** permission */
+    address internal admin;
+    address internal logic;
+    address internal manager;
+}
+contract ERC20 is State {
+    /** permission */
+    modifier onlyAdmin() {
+        require(
+            admin == msg.sender
+        );
+        _;
+    }
+    modifier onlyLogic() {
+        require(
+            logic == msg.sender
+        );
+        _;
+    }
+    modifier onlyManager() {
+        require(
+            manager == msg.sender
+        );
+        _;
+    }
+    /** basic */
+    function name() public view returns (string memory) {return token.name;}
+    function symbol() public view returns (string memory) {return token.symbol;}
+    function decimals() public view returns (uint8) {return token.decimals;}
+    function totalSupply() public view returns (uint256) {return token.totalSupply;}
+    function balanceOf(address _domain) public view returns (uint256) {return balance[_domain];}
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        address _from = msg.sender;
+        require(
+            _value <= balanceOf(_from) &&
+            _value >= 0 &&
+            _from != address(0) &&
+            _to != address(0)
+        );
+        balance[_from] -= _value;
+        balance[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    function mint(address _to, uint256 _value) public onlyLogic returns (bool) {
+        address _from = address(0);
+        require(
+            _value <= token.mintable &&
+            _value >= 0 &&
+            _to != address(0)
+        );
+        token.mintable -= _value;
+        token.totalSupply += _value;
+        balance[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    function burn(address _from, uint256 _value) public onlyLogic returns (bool) {
+        address _to = address(0)
+        require(
+            _value >= 0 &&
+            _value <= balance[_from]
+        );
+        token.totalSupply -= _value;
+        balance[_from] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        uint256 _allowance = allowed[_from][msg.sender];
+        require(
+            _allowance != type(uint256).max &&
+            _allowance >= _value &&
+            _value <= balanceOf(_from) &&
+            _value >= 0 &&
+            _from != address(0) &&
+            _to != address(0)
+        );
+        allowed[_from][msg.sender] -= _value;
+        balance[_from] -= _value;
+        balance[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        address _owner = msg.sender;
+        require(
+            _owner != address(0) &&
+            _spender != address(0)
+        );
+        allowed[_owner][_spender] = _value;
+        emit Approval(_owner, _spender, _value);
+        return true;
+    }
+    function allowance(
+        address _owner, 
+        address _spender
+    ) public view returns (uint256) {return allowed[_owner][_spender];}
+}
+/** pool | fund | index mechanic */
+contract Pool is ERC20 {
+    function contribute() public returns (bool) {
+        require(
+
+        );
+        return true;
+    }
+    /** selling and buying only for manager */
+}
+contract Interface is Pool {
+    function updateLogic(address _domain) public onlyAdmin returns (bool) {
+        logic = _domain;
+        return true;
+    }
+    function updateAdmin(address _domain) public onlyAdmin returns (bool) {
+        admin = _domain;
+        return true;
+    }
+    constructor(
+        address _admin,
+        address _logic,
+        address _manager
+    ) {
+        admin = _admin;
+        logic = _logic;
+        manager = _manager;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+contract Logic {    
+    /**
+    TPV START - HWM = 2000
+    PERFORMANCE FEE 20%
+    2000 * 0.2 = 400
+     */
+    constructor() {
+
+    }
+}
+
+interface ERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256);
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
+    function allowance(address _owner, address _spender) external view returns (uint256);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract Conduit {
+    event Itransfer(address indexed _token, address indexed _to, uint256 _value);
+    event ItransferFrom(address indexed _token, address indexed _from, address indexed _to, uint256 _value);
+    event IApprove(address indexed _token, address indexed _spender, uint256 _value);
+    event IBalanceOf(address indexed _token, address indexed _owner);
+    event IAllowance(address indexed _token, address indexed _owner, address indexed _spender);
+    
+    function Itransfer(address _token, address _to, uint256 _value) internal {
+        require(_token != address(0), "zero address");
+        require(_to != address(0), "zero address");
+        require(_value > 0, "transfer amount is equal or less than zero");
+        IERC20 token = IERC20(_token);
+        try token.transfer(_to, _value) {emit Itransfer(token, _to, _value);}
+        catch Error(string memory message) {revert(message);}
+        catch {revert();}
+    }
+
+    function ItransferFrom(address _token, address sender, address _to, uint256 _value) internal {
+        require(_token != address(0), "zero address");
+        require(_sender != address(0), "zero address");
+        require(_to != address(0), "zero address");
+        require(_value > 0, "transfer amount is equal or less than zero");
+        IERC20 token = IERC20(_token);
+        try token.transferFrom(_sender, _to, _value) {emit ItransferFrom(token, _sender, _to, _value);}
+        catch Error(string memory message) {revert(message);}
+        catch {revert();}
+    }
+
+    function IApprove(address _token, address _spender, uint256 _value) internal {
+        IERC20 token = IERC20(_token);
+        try token.approve(_spender, _value) {emit IApprove(token, _spender, _value);}
+        catch Error(string memory message) {revert(message);}
+        catch {revert();}
+    }
+
+    function IBalanceOf(address _token, address _owner) internal returns (uint256) {
+        IERC20 token = IERC20(_token);
+        try token.balanceOf(_owner) {return token.balanceOf(_owner); emit IBalanceOf(token, _owner);}
+        catch Error(string memory message) {revert(message);}
+        catch {revert();}
+    }
+
+    function IAllowance(address _token, address _owner, address _spender) internal returns (uint256) {
+        IERC20 token = IERC20(_token);
+        try token.allowance(_owner, _spender) {return token.allowance(_owner, _spender); emit IAllowance(token, _owner, _spender);}
+        catch Error(string memory message) {revert(message);}
+        catch {revert();}
+    }
+}
+
+// token a pool creator can issue to represent ownership of the pool also ERC20
+// people must use our native currency as base currency
+contract PoolToken {
+    address creator;
+    string immutable name;
+    string immutable subSymbol;
+    uint8 immutable decimals;
+    uint256 totalSupply;
+    uint256 immutable maxSupply; // if its an open ended fund then it will not require one
+
+    mapping(address => uint256) internal balances;
+
+    mapping(address => bool) internal isCreator;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Mint(address indexed _to, uint256 _value);
+    event Burn(address indexed _to, uint256 _value);
+    modifier creator() {
+        require(isCreator[msg.sender] == true, "unauthorized");
+        _;
+    }
+
+    constructor(string _name, string _subSymbol) {
+        creator = msg.sender;
+        name = _name;
+        subSymbol = _subSymbol;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(balances[msg.sender] >= _value), "insufficient balance";
+        Math.sub(balances[msg.sender], _value);
+        Math.add(balances[_to], _value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function mint(address _to, uint256 _value) internal returns (bool) {
+        require(_to != address(0), "invalid address");
+        require(_value > 0, "invalid value");
+        Math.add(totalSupply, _value);
+        Math.add(balances[_to], _value);
+        emit Mint(_to, _value);
+        return true;
+    }
+
+    function burn(address _to, uint256 _value) internal returns (bool) {
+        require(_value > 0, "invalid value");
+        require(balances[msg.sender] >= _value, "insufficient balance");
+        Math.sub(totalSupply, _value);
+        Math.sub(balances[msg.sender], _value);
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+    // ERC20 STANDARD
+    function name() public view returns (string) {return name;}
+}
+
+// decentralizing the power to create funds?? liquidity too low for a big passive invester but can trade large liquidity
+contract Pool is Conduit, PoolToken { // the funding needs 
+    /*
+    Allowing anyone to start a fund
+     */
+    
+    uint256 i = 0;
+    mapping(uint256 => Fund) internal funds;
+    struct Fund {
+        uint256 index;              // ref no of the fund
+        uint256 aum;                // asset under management
+        uint256 lbt;                // liabilities
+        uint256 nav;                // aum - lbt
+        uint256 navps;              // nav / totalSupply
+        uint256 uniqueContributors; // value of unique address of contributors
+        uint256 feeManagement;      // management fee
+        uint256 feePerformance;     // performance fee
+        uint256 threshold;          // threshold before performance fees apply
+        string name;                // name of the fund
+        string subSymbol;           // ticker of the fund within the DREAM ecosystem
+        uint8 decimals;             // decimals
+        address creator;            // address of the fund creator
+        uint256 creatorStake;       // amount of DREAM the creator has in the fund
+        uint256 fundingRequired;    // amount of DREAM requested for the fund
+        uint256 maxSupply;          // maximum possible contributions
+        uint256 totalSupply;        // total amount of shares of the closed fund being minted
+        bool canTransferOut;        // can this fund transfer funds out of the wallet
+        uint256 minDeposit;         // min deposit per address
+        uint256 maxDeposit;         // max deposit per address
+    }
+    mapping(uint256 => mapping(address => uint256)) fundsBalances;   // amount of fund tokens each address has per fund
+    mapping(uint256 => mapping(address => uint256)) fundsVote;       // some funds may want to have decentralized governance
+    mapping(uint256 => mapping(address => bool)) fundsIsWhitelisted; // some funds may want to white list who can use their fund
+    mapping(uint256 => mapping(address => bool)) fundsIsCreator;     // who is the creator
+
+    modifier newFund() {
+        _;
+        i++;
+    }
+
+    constructor() {}
+
+    function createFund(string memory _name, string memory _subSymbol) external newFund returns (bool) {
+        Fund memory fund = Fund({
+            index: i,
+            aum: 0,
+            lbt: 0,
+            nav: 0,
+            navps: 0,
+            uniqueContributors: 0,
+            feeManagement: _feeManagement,
+            feePerformance: _feePerformance,
+            threshold: _threshold,
+            name: _name,
+            subSymbol: _subSymbol,
+            decimals: 18,
+            creator: msg.sender,
+            creatorStake: 0,
+            fundingRequired: _fundingRequired,
+            maxSupply: _maxSupply,
+            totalSupply: 0,
+            canTransferOut: _canTransferOut,
+            minDeposit: _minDeposit,
+            maxDeposit: _maxDeposit
+        });
+        // the fund creator is creator
+        fundsIsCreator[i][msg.sender] = true;
+        fundsIsWhitelisted[i][msg.sender] = true;
+        // append fund to map
+        funds[i] = fund;
+        return true;
+    }
+
+    function deposit(uint256 _index, uint256 _value) {
+        require(_index <= i, "fund ref not found");
+
+        // transfer x amount of our token to the address
+        require(IBalanceOf(//our contract, msg.sender) >= _value);
+        Itransfer()
+
+    }
+
+    function withdraw() {
+
+    }
+}
---
+++ smart_contracts/contracts/Vault.sol
@@ -0,0 +1,346 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract State {
+    address internal nativeTokenContract;
+    address internal nativeAdminContract;    // governor
+    address internal quickSwapContract;
+    /** map */
+    mapping(string => address)   internal map;
+
+    /** permissions */
+    mapping(address => bool)     internal admin;
+
+    /** accounting */
+    mapping(string => address)   internal holdingsContract;
+    mapping(string => uint256)   internal holdings;
+    mapping(string => uint256)   internal ask;
+    mapping(string => uint256)   internal bid;
+    mapping(string => uint256)   internal available;
+    mapping(string => bool)      internal swappable;
+}
+
+interface IAuthenticator {
+    
+    function grantPermissionAdmin(address _owner) external returns (bool);
+    function revokePermissionAdmin(address _owner) external returns (bool);
+}
+
+interface IERC20 {
+
+    function totalSupply() external view returns (uint256);
+    function allowance(address _owner, address _spender) external view returns (uint256);
+    function approve(address _spender, uint256 _amount) external returns (bool);
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 _amount) external returns (bool);
+    function balanceOf(address _owner) external view returns (uint256);
+    function decimals() external view returns (uint8);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+interface IUniswapV2Router {
+    function swapExactTokensForTokens(
+        uint256 _valueIn,
+        uint256 _valueOutMin,
+        address[] calldata _path,
+        address _to,
+        uint256 _deadline
+    ) external returns (uint256[] memory _values);
+}
+
+interface IVault {
+
+    function swapLocalForMATIC(string _symbol) payable external returns (bool);
+    function swapLocalForToken(string _symbol, uint256 _value) payable external returns (bool);
+    function depositERC20(address _contract, uint256 _value) external returns (bool);
+    function withdrawERC20(address _contract, address _to, uint256 _value) external returns (bool);
+    function depositMATIC(uint256 _value) payable external returns (bool);
+    function withdrawMATIC(address _to, uint256 _value) external returns (bool);
+    function fetchHoldings(string _symbol) external view returns (uint256);
+}
+
+contract Vault is State {
+
+    /**
+    Pre Seed Funding    $0.035
+    Seed Funding        $0.050
+    Series A            $0.250
+    Series B            $0.500
+    ICO                 $1.000
+     */
+    constructor(address _dev) {
+        admin[msg.sender]    = true;
+        admin[_dev]          = true;
+
+        map["UNISWAP_V2_ROUTER"] = 0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff;
+        map["NATIVE_TOKEN"] = address(0);
+        map["NATIVE_ADMIN"] = address(0);
+        holdingsContract["WETH"] = 0x7ceb23fd6bc0add59e62ac25578270cff1b9f619;
+    }
+    /** on polygon */
+    function swapTokenForMATIC(string _symbol) payable external returns (bool) {
+        /** we give x token for x MATIC */
+        address _from = address(this);
+        address _to = msg.sender;
+        uint256 _ask = ask[_symbol];
+        uint256 _swapValue = msg.value / _ask;
+        address _contract = holdingsContract[_symbol];
+        uint256 _holdings = holdings[_symbol];
+        uint256 _available = available[_symbol];
+        bool _swappable = swappable[_symbol];
+        IERC20 _token = IERC20(_contract);
+        require(
+            _swappable &&
+            msg.value > 0 &&
+            _swapValue <= _token.balanceOf(_from) &&
+            _swapValue <= _available &&
+            _ask >= 0 &&
+            _contract != address(0) &&
+            _contract != 0
+        );
+        /** recieve MATIC */
+        holdings["MATIC"] += msg.value;
+        /** send Tokens */
+        available[_symbol] -= _swapValue;
+        holdings[_symbol] -= _swapValue;
+        _token.transfer(_to, _swapValue);
+        return true;
+    }
+    /** on polygon */
+    function swapMATICForToken(string _symbol, uint256 _value) payable external returns (bool) {
+        /** we give x MATIC for x token */
+        address _from = address(this);
+        address _to = msg.sender;
+        uint256 _bid = bid[_symbol];
+        uint256 _swapValue = _value * _bid;
+        address _contract = holdingsContract[_symbol];
+        uint256 _holdings = holdings[_symbol];
+        uint256 _holdingsMATIC = holdings["MATIC"];
+        uint256 _available = available[_symbol];
+        bool _swappable = swappable[_symbol];
+        IERC20 _token = IERC20(_contract);
+        require(
+            _swappable &&
+            msg.value > 0 &&
+            _swapValue <= _holdingsMATIC &&
+            _swapValue <= _available &&
+            _bid > 0 &&
+            _contract != address(0) &&
+            _contract != 0 &&
+        );
+        /** recieve Tokens */
+        _token.transferFrom(_to, _from, _value);
+        holdings[_symbol] += _value
+        /** send MATIC */
+        _from.transfer(_swapValue);
+        available[_symbol] -= _swapValue;
+        holdings["MATIC"] -= msg.value;
+        return true;
+    }
+
+    /**
+    Token in Vault > Uniswap > Token to Vault
+     */
+    function swapOnUniswap(
+        address _contractIn,    // address of token in
+        address _contractOut,   // address of token out
+        uint256 _valueIn,       // amount of token in
+        uint256 _valueOutMin    // min amount of token out
+    ) external returns (bool) {
+        require(
+            admin[msg.sender]
+        );
+        _to = address(this);
+        IERC20 _tokenIn = IERC20(_contractIn);
+        IERC20 _tokenOut = IERC20(_contractOut);
+
+        _tokenIn.transferFrom(msg.sender, address(this), _valueIn);
+        _tokenIn.approve(map["UNISWAP_V2_ROUTER"], _valueIn);
+        
+        address[] memory _path;
+        _path = new address[](3);
+        _path[0] = _contractIn;
+        _path[1] = holdingsContract["WETH"];
+        _path[2] = _contractOut;
+
+        IUniswapV2Router(map["UNISWAP_V2_ROUTER"]).swapExactTokensForTokens(
+            _valueIn,
+            _valueOutMin,
+            _path,
+            _to,
+            block.timestamp
+        );
+    }
+
+    function depositERC20(address _contract, uint256 _value) payable external returns (bool) {
+        IERC20 _token = IERC20(_contract);
+        address _from = msg.sender;
+        address _to = address(this);
+        require(
+            _value <= _token.balanceOf(_from) &&
+            _to != address(0) &&
+            _from != address(0)
+        );
+        bool _success = _token.transferFrom(_from, _to, _value * 10**_token.decimals());
+        cont[_token.symbol()] = _contract;
+        if (_success == true) {holdings[_token.symbol()] += _value;}
+        return _success;
+    }
+
+    function withdrawERC20(address _contract, address _to, uint256 _value) payable external returns (bool) {
+        IERC20 _token = IERC20(_contract);
+        address _from = address(this);
+        require(
+            admin[msg.sender] &&
+            _value <= _token.balanceOf(_from) &&
+            _to != address(0) &&
+            _from != address(0)
+        );
+        bool _success = _token.transfer(_to, _value * 10**_token.decimals());
+        if (_success == true) {holdings[_token.symbol()] -= _value;}
+        return _success;
+    }
+    /** deposit NFTs */
+    function deposit721() payable external returns (bool) {
+
+    }
+
+    function withdraw721() payable external returns (bool) {
+        
+    }
+
+    function depositMATIC(uint256 _value) payable external returns (bool) {
+        holdings["MATIC"] += msg.value;
+        return true;
+    }
+
+    function() payable external returns (bool) {
+        holdings["MATIC"] += msg.value;
+        return true;
+    }
+
+    function withdrawMATIC(address _to, uint256 _value) payable external returns (bool) {
+        require(
+            admin[msg.sender] &&
+            _value <= holdings["MATIC"]
+        );
+        _to.transfer(_value);
+        holdings["MATIC"] -= msg.value;
+        return true;
+    }
+
+    updateHoldingsContract(string _symbol, address _newContract) external returns (bool) {
+        require(
+            admin[msg.sender]
+        );
+        holdingsContract[_symbol] = _newContract;
+        return true;
+    }
+
+    updateAsk(string _symbol, uint256 _ask) external returns (bool) {
+        require(
+            admin[msg.sender] &&
+            _ask >= 0
+        );
+        ask[_symbol] = _ask;
+        return true;
+    }
+
+    updateBid(string _symbol, uint256 _bid) external returns (bool) {
+        require(
+            admin[msg.sender] &&
+            _bid >= 0
+        );
+        bid[_symbol] = _bid;
+        return true;
+    }
+
+    increaseAvailable(string _symbol, uint256 _increase) external returns (bool) {
+        require(
+            admin[msg.sender] &&
+            _increase >= 0
+        );
+        available[_symbol] += _increase;
+        return true;
+    }
+
+    decreaseAvailable(string _symbol, uint256 _decrease) external returns (bool) {
+        require(
+            admin[msg.sender] &&
+            _decrease >= 0 &&
+            available[_symbol] - _decrease >= 0
+        );
+        available[_symbol] -= _decrease;
+        return true;
+    }
+
+    updateAvailable(string _symbol, uint256 _newValue) external returns (bool) {
+        require(
+            admin[msg.sender] &&
+            _newValue >= 0
+        );
+        available[_symbol] = _newValue;
+        return true;
+    }
+
+    updateSwappable(string _symbol, bool _swappable) external returns (bool) {
+        require(
+            admin[msg.sender]
+        );
+        swappable[_symbol] = _swappable;
+        return true;
+    }
+
+    fetchHoldingsContract(string _symbol) external view returns (address) {return holdingsContract[_symbol];}
+    fetchHoldings(string _symbol) external view returns (uint256) {return holdings[_symbol];}
+    fetchAsk(string _symbol) external view returns (uint256) {return ask[_symbol];}
+    fetchBid(string _symbol) external view returns (uint256) {return bid[_symbol];}
+    fetchAvailable(string _symbol) external view returns (uint256) {return available[_symbol];}
+    fetchSwappable(string _symbol) external view returns (uint256) {return swappable[_symbol];}
+
+    function update(
+        address _nativeTokenContract, 
+        address _nativeAdminContract) external returns (bool) {
+        address _from = msg.sender;
+        require(
+            admin[_from] &&
+            _nativeTokenContract != address(0) &&
+            _nativeAdminContract != address(0)
+        );
+        /** revoke admin privilate of current contract */
+        admin[
+            map["NATIVE_TOKEN"]
+        ] = false;
+        /** update contract */
+        map["NATIVE_TOKEN"] = _nativeTokenContract;
+        /** grant admin privilage to new contract */
+        admin[
+            map["NATIVE_TOKEN"]
+        ] = true;
+
+        /** revoke admin privilate of current contract */
+        admin[
+            map["NATIVE_ADMIN"]
+        ] = false;
+        /** update contract */
+        map["NATIVE_ADMIN"] = _nativeAdminContract;
+        /** grant admin privilage to new contract */
+        admin[
+            map["NATIVE_ADMIN"]
+        ] = true;
+
+        return true;
+    }
+
+    function fetch() external returns (address, address) {
+        address _from = msg.sender;
+        require(
+            admin[_from]
+        );
+        return (
+            map["NATIVE_TOKEN"],
+            map["NATIVE_ADMIN"]
+        );
+    }
+}
