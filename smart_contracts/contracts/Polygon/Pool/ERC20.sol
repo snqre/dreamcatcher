5658c783e9cc8789b95dad9ff79ce9651402c102
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-05-01 03:43:13 +0100

    work in progress

--- smart_contracts/contracts/Polygon/Pool/ERC20.sol
+++ smart_contracts/contracts/Polygon/Pool/ERC20.sol
@@ -1,42 +1,41 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
+
 interface IERC20 {
     function name() external view returns (string memory);
+
     function symbol() external view returns (string memory);
+
     function decimals() external view returns (uint8);
+
     function totalSupply() external view returns (uint256);
-    function balanceOf(
-        address _owner
-    ) external view returns (uint256);
-    function transfer(
-        address _to, 
-        uint256 _value
-    ) external returns (bool);
-    function allowance(
-        address _owner, 
-        address _spender
-    ) external view returns (uint256);
-    function approve(
-        address _spender, 
-        uint256 _value
-    ) external returns (bool);
+
+    function balanceOf(address _owner) external view returns (uint256);
+
+    function transfer(address _to, uint256 _value) external returns (bool);
+
+    function allowance(address _owner, address _spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address _spender, uint256 _value) external returns (bool);
+
     function transferFrom(
-        address _from, 
-        address _to, 
+        address _from,
+        address _to,
         uint256 _value
     ) external returns (bool);
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 _value
-    );
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(
         address indexed _owner,
         address indexed _spender,
         uint256 _value
     );
 }
-contract ERC20Cap is IERC20 {
+
+contract ERC20 is IERC20 {
     struct My {
         address admin;
         string name;
@@ -47,14 +46,12 @@ contract ERC20Cap is IERC20 {
         uint256 maxSupply;
     }
     My private my;
-    mapping(address=>uint256) private balance;
-    mapping(address=>mapping(address=>uint256)) private allowed;
+    mapping(address => uint256) private balance;
+    mapping(address => mapping(address => uint256)) private allowed;
     modifier onlyAdmin() {
-        require(
-            my.admin == msg.sender ||
-            address(this) == msg.sender
-        );
+        require(my.admin == msg.sender || address(this) == msg.sender);
     }
+
     constructor(
         address _admin,
         string _name,
@@ -64,9 +61,9 @@ contract ERC20Cap is IERC20 {
     ) {
         require(
             _decimals >= 0 &&
-            _decimals <= 18 &&
-            _admin != address(0) &&
-            _maxSupply >= 1
+                _decimals <= 18 &&
+                _admin != address(0) &&
+                _maxSupply >= 1
         );
         my.admin = _admin;
         my.name = _name;
@@ -76,88 +73,59 @@ contract ERC20Cap is IERC20 {
         my.mintable = _maxSupply;
         my.totalSupply = 0;
     }
+
     function transfer_(
-        address _from, 
-        address _to, 
+        address _from,
+        address _to,
         uint256 _value
     ) private {
         require(
             _from != address(0) &&
-            _to != address(0) &&
-            balance[_from] >= _value &&
-            balance[_from] >= 0 &&
-            _value >= 0
+                _to != address(0) &&
+                balance[_from] >= _value &&
+                balance[_from] >= 0 &&
+                _value >= 0
         );
         balance[_from] -= _value;
         balance[_to] += _value;
         emit Transfer(_from, _to, _value);
     }
-    function mint_(
-        address _to,
-        uint256 _value
-    ) private {
+
+    function mint_(address _to, uint256 _value) private {
         address _from = address(0);
-        require(
-            _value >= 0 &&
-            _value <= my.mintable &&
-            _to != address(0)
-        );
+        require(_value >= 0 && _value <= my.mintable && _to != address(0));
         my.mintable -= _value;
         my.totalSupply += _value;
         balance[_to] += _value;
-        emit Transfer(
-            _from, 
-            _to, 
-            _value
-        );
+        emit Transfer(_from, _to, _value);
     }
-    function burn_(
-        address _from,
-        uint256 _value
-    ) private {
+
+    function burn_(address _from, uint256 _value) private {
         address _to = address(0);
-        require(
-            _value >= 0 &&
-            _value <= balance[_from]
-        );
+        require(_value >= 0 && _value <= balance[_from]);
         my.totalSupply -= _value;
         balance[_from] -= _value;
-        emit Transfer(
-            _from,
-            _to,
-            _value
-        );
+        emit Transfer(_from, _to, _value);
     }
-    function mint(
-        address _to,
-        uint256 _value
-    ) public onlyAdmin returns (bool) {
-        _mint(
-            _to,
-            _value
-        );
+
+    function mint(address _to, uint256 _value) public onlyAdmin returns (bool) {
+        _mint(_to, _value);
     }
-    function burn(
-        address _from,
-        uint256 _value
-    ) public onlyAdmin returns (bool) {
-        _mint(
-            _to,
-            _value
-        );
+
+    function burn(address _from, uint256 _value)
+        public
+        onlyAdmin
+        returns (bool)
+    {
+        _mint(_to, _value);
     }
-    function transfer(
-        address _to,
-        uint256 _value
-    ) public returns (bool) {
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
         address _from = msg.sender;
-        transfer_(
-            _from,
-            _to,
-            _value
-        );
+        transfer_(_from, _to, _value);
         return true;
     }
+
     function transferFrom(
         address _from,
         address _to,
@@ -165,47 +133,49 @@ contract ERC20Cap is IERC20 {
     ) public returns (bool) {
         address _spender = msg.sender;
         uint256 _allwnc = allowed[_from][_spender];
-        require(
-            _allwnc != type(uint256).max &&
-            _allwnc >= _value
-        );
+        require(_allwnc != type(uint256).max && _allwnc >= _value);
         allowed[_from][_spender] -= _value;
-        transfer_(
-            _from,
-            _to,
-            _value
-        );
+        transfer_(_from, _to, _value);
         return true;
     }
-    function approve(
-        address _spender,
-        uint256 _value
-        
-    ) public returns (bool) {
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
         address _owner = msg.sender;
-        require(
-            _owner != address(0) &&
-            _spender != address(0) &&
-            _value >= 0
-        );
+        require(_owner != address(0) && _spender != address(0) && _value >= 0);
         allowed[_owner][_spender] = _value;
-        emit Approval(
-            _owner,
-            _spender,
-            _value
-        );
+        emit Approval(_owner, _spender, _value);
         return true;
     }
-    function name() public view returns (string memory) {return my.name;}
-    function symbol() public view returns (string memory) {return my.symbol;}
-    function decimals() public view returns (uint8) {return my.decimals;}
-    function maxSupply() public view returns (uint256) {return my.maxSupply;}
-    function mintable() public view returns (uint256) {return my.mintable;}
-    function balanceOf(
-        address _owner
-    ) public view returns (uint256) {return balance[_owner];}
-    function allowance(
-        address _owner, 
-        address _spender
-    ) public view returns (uint256) {return allowed[_owner][_spender];}
+
+    function name() public view returns (string memory) {
+        return my.name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return my.symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return my.decimals;
+    }
+
+    function maxSupply() public view returns (uint256) {
+        return my.maxSupply;
+    }
+
+    function mintable() public view returns (uint256) {
+        return my.mintable;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balance[_owner];
+    }
+
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256)
+    {
+        return allowed[_owner][_spender];
+    }
 }
--- smart_contracts/contracts/Polygon/Pool/PoolClosedv1.sol
+++ smart_contracts/contracts/Polygon/Pool/PoolClosedv1.sol
@@ -17,42 +17,12 @@ contract State {
     uint256 fundingEnd;
     uint256 requestedWei;
     uint256 requiredWei;
+    mapping(address => bool) internal whitelist;
 }
 
 contract PoolClosedv1 is State {
-    struct My {
-        string name;
-        uint256 balanceWei;
-        ERC20Cap nativeToken;
-        address creator;
-        address manager;
-    }
-    struct Settings {
-        bool isWhitelistOnly;
-    }
-    // =.=.= LIFE CYCLE OF A POOL =.=.=
-    struct Funding {
-        uint256 start;
-        uint256 minDuration;
-        uint256 maxDuration;
-        uint256 duration;
-        uint256 close;
-        uint256 requestedWei;
-        uint256 requiredWei;
-    }
-    My private my;
-    Settings private settings;
-    Funding private funding;
-    mapping(address => bool) private whitelist;
-    mapping(address => uint256) private holdings;
-    modifier onlyWhitelisted() {
-        require(isWhitelistOnly && whitelist[msg.sender]);
-        _;
-    }
-    modifier onlyCreator() {
-        require(my.creator == msg.sender);
-        _;
-    }
+
+
 
     constructor(
         address _manager,
@@ -64,10 +34,11 @@ contract PoolClosedv1 is State {
         uint256 _tknInitialSupply,
         uint256 _fundingDuration
     ) {
+        
         require(
             msg.value >= 1 &&
-                _tknInitialSupply <= _tknMaxSupply &&
-                _fundingDuration >= 1 weeks
+            _tknInitialSupply <= _tknMaxSupply &&
+            _fundingDuration >= 1 weeks
         );
         my.creator = msg.sender;
         my.manager = _manager;
@@ -84,6 +55,18 @@ contract PoolClosedv1 is State {
         funding.close = funding.start + _fundingDuration;
         my.balanceWei += msg.value;
         mint(my.creator, _tknInitialSupply);
+    }
+
+    function setUp(
+        address _manager,
+        string _name,
+        string _description,
+        
+    ) public returns (bool) {
+        require(
+            msg.sender == creator
+        );
+        
     }
 
     function contribute() public onlyWhitelisted returns (bool) {
