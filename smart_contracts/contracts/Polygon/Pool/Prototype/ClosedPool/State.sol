c45ff49e08a4f5dd266f274f32ba66401be37fc4
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-05-02 01:58:57 +0100

    final work for today

--- smart_contracts/contracts/Polygon/Pool/Prototype/ClosedPool/State.sol
+++ smart_contracts/contracts/Polygon/Pool/Prototype/ClosedPool/State.sol
@@ -6,9 +6,27 @@ contract State {
     mapping(address => uint256) internal contribution;
     mapping(address => bool) internal isManager;
     mapping(address => bool) internal isAdmin;
+    mapping(address => bool) internal whitelisted;
+
+    struct Toggles {
+        bool extensions; // allow matic to be transfered to external contracts
+        bool whitelist;  // only whitelisted addresses can contribute to the pool
+    }
+
+    Toggles internal toggles;
+
+    struct InitialFunding {
+        uint256 start;
+        uint256 duration;
+        uint256 required;   // wei
+    }
+
+    InitialFunding internal initialFunding;
+
     modifier manager() {
         require(isManager[msg.sender], "State: msg.sender != manager");
     }
+
     modifier admin() {
         require(isAdmin[msg.sender], "State: msg.sender != admin");
     }
@@ -55,5 +73,42 @@ contract State {
 
     function contributionOf(address _account) public returns (uint256) {
         return contribution[_account];
+    }
+
+    function setWhitelist(address _account, bool _state) public admin returns (bool) {
+        whitelisted[_account] = _state;
+        return true;
+    }
+
+    function whitelistedOf(address _account) public returns (bool) {
+        return whitelisted[_account];
+    }
+
+    function setToggles(
+        bool _extensions,
+        bool _whitelist
+    ) public admin returns (bool) {
+        toggles.extensions = _extensions;
+        toggles.whitelist = _whitelist;
+    }
+
+    function getToggles() public returns (
+        bool,
+        bool
+    ) {
+        return (
+            toggles.extensions,
+            toggles.whitelist
+        );
+    }
+
+    function setInitialFunding(
+        uint256 _start,
+        uint256 _duration,
+        uint256 _required
+    ) public admin returns (bool) {
+        initialFunding.start = _start;
+        initialFunding.duration = _duration;
+        initialFunding.required = _required;
     }
 }
