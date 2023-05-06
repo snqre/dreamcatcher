a155540acda62dfc9714c39040714fbde8aea47d
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-05-06 21:37:39 +0100

    Update Pool.sol

--- blockchain/contracts/Polygon/Pool/Prototype/ClosedPool/Pool.sol
+++ blockchain/contracts/Polygon/Pool/Prototype/ClosedPool/Pool.sol
@@ -1,26 +1,86 @@
 pragma solidity ^0.8.0;
 import "smart_contracts/contracts/Polygon/ERC20Standards/Token.sol";
 
-/**
-* seams like the main errors encountered on testnet are permissions
- */
 contract Pool {
+    struct Funding {
+        uint256 start;
+        uint256 duration;
+        uint256 end;
+        uint256 min;
+        uint256 max;
+        bool onlyWhitelist;
+    }
+
+    struct Settings {
+        bool creatorCanTransfer;
+    }
+
+    address creator;
+    
+    Funding private funding;
+    mapping(address => bool) private whitelisted;
+
     Token nativeToken;
+
+    modifier wht() {
+        require(
+            funding.onlyVerified &&
+            verified[msg.sender],
+            "funding.onlyVerified != true || msg.sender != verified"
+        );
+        _;
+    }
+
+    modifier crt() {
+        require(
+            msg.sender == creator
+        );
+        _;
+    }
 
     constructor (
         string memory _tknName,
         string memory _tknSymbol,
-        uint256 _tknInitialSupply
+        uint256 _tknInitialSupply,
+        uint256 _fundingDuration,
+        uint256 _fundingMin,
+        uint256 _fundingMax,
+        bool _onlyWhitelist,
+        bool _creatorCanTransfer
     ) payable {
-        require(msg.value >= 0.01 * 10**18, "Pool: msg.value < 1 * 10**18");
-        require(_tknInitialSupply * 10**18 >= 1 * 10**18, "Pool: _tknInitialSupply < 1 * 10**18");
+        uint256 _start = block.timestamp;
+        uint256 _end = _start + _fundingDuration;
+        uint256 _minMaticDeployment = 0.01 * 10**18;
+        uint256 _minInitialSupply = 1 * 10**18;
+        uint256 _minFundingDuration = 1 weeks;
+        uint256 _maxFundingDuration = 1 years;
         address _creator = msg.sender;
+
+        require(
+            msg.value >= _minMaticDeployment &&
+            _tknInitialSupply >= _minInitialSupply &&
+            _fundingDuration >= _minFundingDuration &&
+            _fundingDuration <= _maxFundingDuration &&
+            _fundingMin >= 0 &&
+            _creator != address(0)
+        );
+
+        creator = _creator;
+        funding.start = _start;
+        funding.end = _start + _fundingDuration;
+        funding.duration = _fundingDuration;
+        funding.min = _fundingMin;
+        funding.max = _fundingMax;
+        funding.onlyWhitelist = _onlyWhitelist;
+
+
+        /** create token contract & deploy intial supply to creator for their initial contribution */
         nativeToken = new Token(_tknName, _tknSymbol);
-        nativeToken.mint(_creator, _tknInitialSupply);
+        nativeToken.mint(creator, _tknInitialSupply);
     }
     
     // ** are we adding value before checking balance?
-    function contribute() public payable returns (bool) {
+    function contribute() wht public payable returns (bool) {
         uint256 _valueWei = msg.value;
         uint256 _supplyWei = nativeToken.totalSupply() / 10**18;
         uint256 _balanceWei = address(this).balance - _valueWei;
@@ -44,6 +104,33 @@ contract Pool {
         address payable _withdrawer = payable(msg.sender);
         nativeToken.burn(msg.sender, _tknValue);
         _withdrawer.transfer(_amountToSend);
+        return true;
+    }
+
+    /** can set white list but will only work if whitelisted only settings is true */
+    function setWhitelistOf(address _account, bool _state) public crt returns (bool) {
+        whitelisted[_account] = _state;
+        return true;
+    }
+
+    function whitelistOf(address _account) public view {
+        return whitelisted[_accoun];
+    }
+
+    function transfer(address _account, uint256 _value) public crt returns (bool) {
+        address payable _recipient = payable(_account);
+        require(
+            creatorCanTransfer &&
+            _recipient != address(0) &&
+            _value >= 0
+        );
+
+        _recipient.transfer(_value);
+        return true;
+    }
+
+    /** recieve matic but will not mint tokens */
+    function recieve() public payable crt returns (bool) {
         return true;
     }
 }
