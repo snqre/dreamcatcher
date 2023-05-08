248986ca498ada5c2956f67a57d45f374f82eb07
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-05-09 00:39:05 +0100

    More work

--- blockchain/contracts/Polygon/Dreamcatcher/State.sol
+++ blockchain/contracts/Polygon/Dreamcatcher/State.sol
@@ -1,25 +1,21 @@
 pragma solidity ^0.8.0;
 
+/** basically the only thing this does it keep a database */
 interface IState {
     function feeToFoundNewPool() public view returns (uint256);
 }
 
 contract State {
-    struct Code {
-        address main;
-    } Code private code;
-    address admin;
-    struct ElectionSchedule {
-        uint256 start;
-        uint256 duration;
-        uint256 reset;
-    } ElectionSchedule private electionSchedule;
+    address terminal;
+    
+    mapping(string => uint256) private stringToUint256;
+    
+    modifier onlyTerminal() {
+        require(msg.sender == terminal || msg.sender == address(this));
+        _;
+    }
 
-    struct Fee {
-        uint256 toFoundNewPool;
-    } Fee private fee;
-
-    function feeToFoundNewPool() public view returns (uint256) {
-        return fee.toFoundNewPool;
+    constructor() {
+        
     }
 }
