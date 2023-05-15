185a21f85c6664b27ebe7e094c0b839467982be0
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-05-15 19:40:12 +0100

    Update token.sol

--- blockchain/contracts/Polygon/Pool/Prototype/pool/token.sol
+++ blockchain/contracts/Polygon/Pool/Prototype/pool/token.sol
@@ -7,33 +7,23 @@ contract Token is IERC20, ERC20 {
     address private owner;
 
     modifier only_owner() {
-        address _operator =msg.sender;
-        require(_operator ==owner);
+        require(msg.sender ==owner);
         _;
     }
 
-    constructor(
-        string memory _name,
-        string memory _symbol
-    ) ERC20(
-        _name,
-        _symbol
-    ) {
-        owner =msg.sender;
-    }
-
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {owner =msg.sender;}
+    
     function mint(
         address _to,
         uint256 _value_in_wei
 
-    ) public only_owner returns (bool) {
+    ) public only_owner {
         require(_to !=address(0));
         require(_value_in_wei >=0);
-        uint256 _value =_value_in_wei *10 **18;
-
+        
         super._mint(
             _to,
-            _value
+            _value_in_wei *10 **18
         );
 
         return true;
