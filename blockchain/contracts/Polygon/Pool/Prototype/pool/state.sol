9e0103849d754122673d6523c3a43a487dd16ad6
snqre <marco.bizxaro@gmail.com>
2023-05-18 04:22:12 +0100

    testing

--- blockchain/contracts/Polygon/Pool/Prototype/pool/state.sol
+++ blockchain/contracts/Polygon/Pool/Prototype/pool/state.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "blockchain/contracts/Polygon/Pool/Prototype/pool/token.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "blockchain/contracts/Polygon/Pool/Prototype/pool/utils.sol";
 
 contract State {
     struct Lock {bool isUnlocked;} Lock private lock;
@@ -16,7 +17,7 @@ contract State {
     }
 
     struct Pool {
-        uint256 reference;
+        uint256 ref;
         uint256 balance;
         Token token;
         FundingSchedule fundingSchedule;
@@ -53,7 +54,7 @@ contract State {
     function createNewPool(
         string memory _name,
         string memory _description,
-        address[] _managers,
+        address[] memory _managers,
         string memory _nameOfToken,
         string memory _symbolOfToken,
         uint256 _duration,
@@ -64,21 +65,21 @@ contract State {
         require(_required >= 0, "_required < 0");
         
         if (priceTo.createNewPool > 0) {
-            IERC20 _nativeToken = IERC20(_nativeToken);
+            IERC20 _nativeToken = IERC20(nativeToken);
             _nativeToken.transferFrom(
                 msg.sender,
                 address(this),
-                Utils.convertToWei(priceTo.createNewPool);
+                Utils.convertToWei(priceTo.createNewPool)
             );
         }
 
         tracker.numberOfPools += 1;
-        uint256 _reference = tracker.numberOfPools;
+        uint256 _ref = tracker.numberOfPools;
 
-        Token _token = new Token(_nameOfToken, _symbolOfToken);
+        Token _token = new Token(_nameOfToken, _symbolOfToken, 18);
 
-        _now = block.timestamp;
-        FundingSchedule _fundingSchedule = FundingSchedule({
+        uint256 _now = block.timestamp;
+        FundingSchedule memory _fundingSchedule = FundingSchedule({
             start: _now,
             end: _now + _duration,
             required: _required,
@@ -86,8 +87,8 @@ contract State {
             success: false
         });
 
-        pools[_reference] = Pool({
-            reference: _reference,
+        pools[_ref] = Pool({
+            ref: _ref,
             balance: msg.value,
             token: _token,
             fundingSchedule: _fundingSchedule
@@ -96,14 +97,14 @@ contract State {
         return true;
     }
 
-    function contribute(uint256 _reference) public payable returns (bool) {
+    function contribute(uint256 _ref) public payable returns (bool) {
         require(msg.value > 0, "msg.value <= 0");
         
         Account memory _caller = accounts[msg.sender];
-        Pool memory _pool = pools[_reference];
+        Pool memory _pool = pools[_ref];
 
         if (_pool.fundingSchedule.whitelisted) {
-            require(_caller.isOnWhitelist[_reference], "this pool is whitelisted and you are not on it");
+            require(_caller.isOnWhitelist[_ref], "this pool is whitelisted and you are not on it");
         }
 
         uint256 _v = msg.value;
@@ -122,17 +123,17 @@ contract State {
         _pool.token.mint(_m);
         _pool.token.transfer(msg.sender, _m);
 
-        pools[_reference] = _pool;
+        pools[_ref] = _pool;
         accounts[msg.sender] = _caller;
 
         return true;
     }
 
-    function withdraw(uint256 _reference, uint256 _value) public returns (bool) {
+    function withdraw(uint256 _ref, uint256 _value) public returns (bool) {
         require(_value > 0, "_value <= 0");
 
         Account memory _caller = accounts[msg.sender];
-        Pool memory _pool = pools[_reference];
+        Pool memory _pool = pools[_ref];
 
         uint256 _a = _value;
         uint256 _s = _pool.token.totalSupply();
