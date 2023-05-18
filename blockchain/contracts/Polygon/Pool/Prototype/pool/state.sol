f3fcdeaf3e1bfdda59241ce56b00a0b3259ff7ed
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-05-18 04:06:14 +0100

    work work work!

--- blockchain/contracts/Polygon/Pool/Prototype/pool/state.sol
+++ blockchain/contracts/Polygon/Pool/Prototype/pool/state.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "blockchain/contracts/Polygon/Pool/Prototype/pool/token.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
@@ -33,34 +33,50 @@ contract State {
     mapping(address => Account) private accounts;
     mapping(uint256 => Pool) private pools;
 
+    address nativeToken;
+
+    struct PriceTo {
+        uint256 createNewPool;
+    }
+
+    PriceTo priceTo;
+
+    struct FeeTo {
+        uint256 contribute;
+        uint256 withdraw;
+    }
+
+    FeeTo feeTo;
+
     constructor() {}
 
     function createNewPool(
         string memory _name,
         string memory _description,
         address[] _managers,
-        string _nameToken,
-        string _symbolToken,
+        string memory _nameOfToken,
+        string memory _symbolOfToken,
         uint256 _duration,
         uint256 _required,
         bool _whitelisted
     ) public payable returns (bool) {
-        require(lock.isUnlocked);
-        /** moderation */
-        require(_duration >= 1 days);
-        require(_required >= 0);
-
-        lock.isUnlocked = false;
+        require(_duration >= 1 days, "funding phase must last more than 24 hours");
+        require(_required >= 0, "_required < 0");
         
-        Account _caller = accounts[msg.sender];
-        /** assign new reference */
-        tracker.numberOfPools ++;
+        if (priceTo.createNewPool > 0) {
+            IERC20 _nativeToken = IERC20(_nativeToken);
+            _nativeToken.transferFrom(
+                msg.sender,
+                address(this),
+                Utils.convertToWei(priceTo.createNewPool);
+            );
+        }
+
+        tracker.numberOfPools += 1;
         uint256 _reference = tracker.numberOfPools;
-        
-        /** create erc20 token contract */
-        Token _token = new Token(_nameToken, _symbolToken);
 
-        /** create funding schedule for launch */
+        Token _token = new Token(_nameOfToken, _symbolOfToken);
+
         _now = block.timestamp;
         FundingSchedule _fundingSchedule = FundingSchedule({
             start: _now,
@@ -70,7 +86,6 @@ contract State {
             success: false
         });
 
-        /** assign ownership of pool to creator address */
         pools[_reference] = Pool({
             reference: _reference,
             balance: msg.value,
@@ -78,56 +93,62 @@ contract State {
             fundingSchedule: _fundingSchedule
         });
 
-        lock.isUnlocked = true;
         return true;
     }
 
     function contribute(uint256 _reference) public payable returns (bool) {
-        // matic > contract > token
-        Account _caller = accounts[msg.sender];
-        Pool _pool = pools[_reference];
+        require(msg.value > 0, "msg.value <= 0");
         
-        if (_pool.fundingSchedule.whitelisted) {require(_caller.isOnWhitelist[_reference]);}
-        /**
-        * _v: value
-        * _s: supply
-        * _b: balance
-        * _m: amount to mint
-         */
+        Account memory _caller = accounts[msg.sender];
+        Pool memory _pool = pools[_reference];
+
+        if (_pool.fundingSchedule.whitelisted) {
+            require(_caller.isOnWhitelist[_reference], "this pool is whitelisted and you are not on it");
+        }
+
         uint256 _v = msg.value;
-        uint256 _s = _pool.token.totalSupply() / 10**18;/** */
+        uint256 _s = Utils.convertToInt(_pool.token.totalSupply());
         uint256 _b = _pool.balance;
-        uint256 _m = Lib._how_much_to_mint(_v, _s, _b);//** */
-
-        require(_v > 0);
-        require(_s > 0);
-        require(_b > 0);
+        uint256 _m = Utils.howMuchToMint(_v, _s, _b);
         
-        require(block.timestamp <= _pool.fundingSchedule.end);
+        require(_v > 0, "_v <= 0");
+        require(_s > 0, "_s <= 0");
+        require(_b > 0, "_b <= 0");
 
-        _pool.token.mint(msg.sender, _m);
-        // accounting
+        require(block.timestamp <= _pool.fundingSchedule.end, "the contribution period for this pool has expired");
+
         _pool.balance += msg.value;
-
-        return true;   
-    }
-
-    function withdraw(uint256 _reference, uint256 _value) public returns (bool) {
-        // token > contract > matic
-        Account _caller = accounts[msg.sender];
-        Pool _pool = pools[_reference];
         
-        // require that funding is still ongoing
-        require(block.timestamp <= _pool.fundingSchedule.end);
-        
-        // burn token supply
-        _pool.token.burn(msg.sender, _value);
+        _pool.token.mint(_m);
+        _pool.token.transfer(msg.sender, _m);
 
-        // transfer matic to them
-        address payable _to = payable(msg.sender);
-        _to.transfer(_value);
+        pools[_reference] = _pool;
+        accounts[msg.sender] = _caller;
 
         return true;
     }
 
+    function withdraw(uint256 _reference, uint256 _value) public returns (bool) {
+        require(_value > 0, "_value <= 0");
+
+        Account memory _caller = accounts[msg.sender];
+        Pool memory _pool = pools[_reference];
+
+        uint256 _a = _value;
+        uint256 _s = _pool.token.totalSupply();
+        uint256 _b = _pool.balance;
+        uint256 _v = Utils.howMuchToSend(_a, _s, _b);
+
+        require(_pool.balance >= _v, "insufficient balance to send to withdrawer");
+
+        _pool.token.transferFrom(msg.sender, address(this), _value);
+        _pool.token.burn(_value);
+        
+        _pool.balance -= _v;
+
+        address payable _to = payable(msg.sender);
+        _to.transfer(_v);
+
+        return true;
+    }
 }
