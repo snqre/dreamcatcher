7b129842a1e62484e3eb78b4de4abedb7ec1c0d2
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-05-19 20:25:16 +0100

    clean up

--- blockchain/contracts/Polygon/Market/Interfaces/IWETH.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-interface IWETH {
-    function deposit() external payable;
-
-    function transfer(address _to, uint256 _value) external returns (bool);
-
-    function withdraw(uint256) external;
-}
