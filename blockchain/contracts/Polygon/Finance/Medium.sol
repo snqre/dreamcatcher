31a5a2eeb54e68fa5c08399c9a8a02e97081c480
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-05-23 00:11:26 +0100

    Update Medium.sol

--- blockchain/contracts/Polygon/Finance/Medium.sol
+++ blockchain/contracts/Polygon/Finance/Medium.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 /** chainlink */
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
