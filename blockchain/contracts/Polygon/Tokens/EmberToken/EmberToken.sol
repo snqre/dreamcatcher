88aa89c268cf874093495b247b8e71d0c986e846
snqre <marco.bizxaro@gmail.com>
2023-05-30 11:33:12 +0100

    testing

--- blockchain/contracts/Polygon/Tokens/EmberToken/EmberToken.sol
+++ blockchain/contracts/Polygon/Tokens/EmberToken/EmberToken.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract EmberToken is
