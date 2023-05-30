5a71f1bf420c3e1d5425f9c6c2ae41a647df5b55
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-05-30 10:30:14 +0100

    Update EmberToken.sol

--- blockchain/contracts/Polygon/Tokens/EmberToken/EmberToken.sol
+++ blockchain/contracts/Polygon/Tokens/EmberToken/EmberToken.sol
@@ -5,50 +5,39 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-
-/**
-
-    theoretical maxSupply is 2000 $ember 
-    200,000,000 $dream
-    2,000 $ember
-
- */
 
 contract EmberToken is
 ERC20,
 ERC20Burnable,
 ERC20Snapshot,
 Ownable,
+Pausable,
 ERC20Permit,
 ERC20Votes {
-
     constructor() ERC20(
         "EmberToken",
         "EMBER"
     ) ERC20Permit(
         "EmberToken"
-    ) Ownable() {}
+    ) {}
 
     /** -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.- */
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
-    ) internal override(
+    ) internal whenNotPaused override(
         ERC20,
         ERC20Snapshot
     ) {
-
         super._beforeTokenTransfer(
             from,
             to,
             amount
         );
-
     }
 
     function _afterTokenTransfer(
@@ -59,13 +48,11 @@ ERC20Votes {
         ERC20,
         ERC20Votes
     ) {
-
         super._afterTokenTransfer(
             from,
             to,
             amount
         );
-
     }
 
     function _mint(
@@ -75,12 +62,10 @@ ERC20Votes {
         ERC20,
         ERC20Votes
     ) {
-
         super._mint(
             to,
             amount
         );
-
     }
 
     function _burn(
@@ -90,47 +75,32 @@ ERC20Votes {
         ERC20,
         ERC20Votes
     ) {
-
         super._burn(
             account,
             amount
         );
-
     }
 
     /** -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.- */
     function snapshot() public onlyOwner {
-
         _snapshot();
+    }
 
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
     }
 
     function mint(
         address to,
         uint256 amount
     ) public onlyOwner {
-
         _mint(
             to,
             amount
         );
-
     }
-
-    function renounceOwnership() public override onlyOwner {
-
-        super.renounceOwnership();
-
-    }
-
-    function transferOwnership(
-        address newOwner
-    ) public override onlyOwner {
-
-        super.transferOwnership(
-            newOwner
-        );
-
-    }
-
 }
