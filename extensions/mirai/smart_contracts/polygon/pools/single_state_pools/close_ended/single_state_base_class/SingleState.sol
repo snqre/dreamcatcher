8eec25e0b65e66b1298ef9f7152906fd45fc97b9
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-06-05 02:13:06 +0100

    Update SingleState.sol

--- extensions/mirai/smart_contracts/polygon/pools/single_state_pools/close_ended/single_state_base_class/SingleState.sol
+++ extensions/mirai/smart_contracts/polygon/pools/single_state_pools/close_ended/single_state_base_class/SingleState.sol
@@ -15,54 +15,8 @@ import "blockchain/contracts/Polygon/ERC20Standards/Tokens/SimpleToken.sol" as S
 import "blockchain/contracts/Polygon/Finance/Medium.sol";
 
 // we use this for centralized pools because they dont need their dedicated voting mechanism
-contract StandardToken is ERC2O, ERC20Burnable, ERC20Permit, AccessControl {
-    using SafeMath for uint256;
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
-        super._beforeTokenTransfer(from, to, amount);
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _mint(to, amount);
-    }
-
-    function burn(address to, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _burn(to, amount);
-    }
-}
-
-// we use this for decentralized and hybrid style pools as they have voting features
-contract GovernanceToken is ERC20, ERC20Burnable, ERC20Snapshot, ERC20Permit, AccessControl {
-    using SafeMath for uint256;
-
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) ERCPermit(name) {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Snapshot) {
-        super._beforeTokenTransfer(from, to, amount);
-    }
-
-    function snapshot() public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _snapshot();
-    }
-
-    function mint(address to, uint256 amount) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _mint(to, amount);
-    }
-
-    function getVotes(address account) public view returns (uint256) {
-        return balanceOfAt(account, _getCurrentSnapshotId());
-    }
-
-    function getPastVotes(address account, uint256 snapshotId) public view returns (uint256) {
-        return balanceOfAt(account, snapshotId);
-    }
-}
 
 interface ISingleState {
     event NewPoolCreated(
