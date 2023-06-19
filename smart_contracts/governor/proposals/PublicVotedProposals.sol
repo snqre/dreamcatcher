094722d6362e80a0569586e849fe2a3a9ce5856e
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-06-19 21:53:54 +0100

    Update PublicVotedProposals.sol

--- smart_contracts/governor/proposals/PublicVotedProposals.sol
+++ smart_contracts/governor/proposals/PublicVotedProposals.sol
@@ -36,12 +36,28 @@ contract PublicVotedProposals is Context, Ownable, ReentrancyGuard {
         address target;
         string signature;
         bytes args;
+        EnumerableSet.
+            AddressSet voters;
     }
 
     mapping(uint => PublicVotedProposal) private publicVotedProposals;
 
+    //account => reference_ : side
+    mapping(address => mapping(uint => uint)) private sideOf;
+
     constructor(address owner) Ownable(owner) {}
 
+    function _hasVoted(uint reference_) internal view virtual returns (bool) {
+        if (publicVotedProposals[reference_].voters.contains(msg.sender)) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    //will replace this once authenticator is complete in terminal
     function _mustNotBeMember(
         uint snapshotId
     ) internal view virtual {
@@ -53,7 +69,7 @@ contract PublicVotedProposals is Context, Ownable, ReentrancyGuard {
             "PublicVotedProposals: caller is a member of referenced member"
         );
     }
-
+    //will replace this once authenticator is complete in terminal
     function _mustBeMember(
         uint snapshotId
     ) internal view virtual {
@@ -89,9 +105,29 @@ contract PublicVotedProposals is Context, Ownable, ReentrancyGuard {
     function _mustBeImplemented(uint reference_) internal view virtual {
         require(publicVotedProposals[reference_].hasBeenImplemented, "PublicVotedProposals: referenced proposal has not been implemented");
     }
-    //required quorum wip
-    function _requiredQuorumHasBeenMet(uint reference_) internal view virtual {
-        uint currentQuorum = (publicVotedProposals[reference_])
+
+    function _mustNotBeExpired(uint reference_) internal view virtual {
+        require(block.timestamp < publicVotedProposals[reference_].endTimestamp, "PublicVotedProposals: referenced proposal has expired");
+    }
+
+    function _requiredQuorumHasBeenMet(uint reference_) internal view virtual returns (bool) {
+        uint quorum = publicVotedProposals[reference_].quorum;
+        uint quorumRequired = publicVotedProposals[reference_].quorumRequired;
+        if (quorum >= quorumRequired) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    function _mustBePresent(uint reference_) internal view virtual {
+        require(
+            reference_ >= 1 &&
+            reference_ <= count,
+            "PublicVotedProposals: reference does not point to an existing proposal"
+        );
     }
 
     function _getAverageActiveQuorum(
@@ -197,6 +233,71 @@ contract PublicVotedProposals is Context, Ownable, ReentrancyGuard {
         );
     }
 
+    function _vote(uint reference_, uint side) internal virtual nonReentrant {
+        PublicVotedProposal storage proposal = publicVotedProposals[reference_];
+        _mustBePresent(reference_);
+        _mustBeMember(proposal.snapshotId);
+        _mustNotBeCleared(reference_);
+        _mustNotBeWithdrawn(reference_);
+        _mustNotBeExpired(reference_);
+
+        uint votes = IDreamToken(dreamToken).getVotesAt(
+            msg.sender,
+            proposal.snapshotId
+        );
+
+        if (_hasVoted(reference_)) {//caller has already voted
+            //clear their votes from existing votes
+            uint side_ = sideOf[msg.sender][reference_];
+            if (side_ == 0) {// abstain
+                proposal.votesToAbstain -= votes;
+            }
+
+            else if (side_ == 1) {//for
+                proposal.votesFor -= votes;
+            }
+
+            else if (side_ == 2) {//against
+                proposal.votesAgainst -= votes;
+            }
+            //add edited votes for new side
+            if (side == 0) {//abstain
+                sideOf[msg.sender][reference_] = 0;
+                proposal.votesToAbstain += votes;
+            }
+
+            else if (side == 1) {//for
+                sideOf[msg.sender][reference_] = 1;
+                proposal.votesFor += votes;
+            }
+
+            else if (side == 2) {//against
+                sideOf[msg.sender][reference_] = 2;
+                proposal.votesAgainst += votes;
+            }
+        }
+
+        else {//caller has not voted yet
+            proposal.voters.add(msg.sender);
+            proposal.quorum += votes;
+            if (side == 0) {//abstain
+                sideOf[msg.sender][reference_] = 0;
+                proposal.votesToAbstain += votes;
+            }
+
+            else if (side == 1) {//for
+                sideOf[msg.sender][reference_] = 1;
+                proposal.votesFor += votes;
+            }
+
+            else if (side == 2) {//against
+                sideOf[msg.sender][reference_] = 2;
+                proposal.votesAgainst += votes;
+            }
+        }
+        
+    }
+
     function pushNewPublicVotedProposal(
         string memory reason,
         uint startTimestamp,
@@ -222,5 +323,17 @@ contract PublicVotedProposals is Context, Ownable, ReentrancyGuard {
             signature,
             args
         );
+    }
+
+    function vote(
+        uint reference_, 
+        uint side
+    ) public virtual nonReentrant returns (bool) {
+        _vote(
+            reference_,
+            side
+        );
+
+        return true;
     }
 }
