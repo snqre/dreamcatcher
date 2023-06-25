16a047bed05452bde30f82bdc8b4806ced06fc67
Marco Bizzaro <marco.bizxaro@gmail.com>
2023-06-25 17:30:32 +0100

    Update Module.sol

--- smart_contracts/module_architecture/Module.sol
+++ smart_contracts/module_architecture/Module.sol
@@ -4,3 +4,30 @@ pragma solidity ^0.8.9;
 interface IModule {
     function setModuleManagerImplementation(address newImplementation) external;
 }
+
+contract Module {
+    uint identifier;
+    uint version;
+    string name;
+    string description;
+    bool isActiveImplementation;
+    address moduleManagerImplementation;
+
+    constructor() {}
+
+    function mustBeActiveImplementation() external view {
+        require(
+            isActiveImplementation,
+            "Not an active implementation."
+        );
+    }
+
+    function setModuleManagerImplementation(address newImplementation) external {
+        moduleManagerImplementation = newImplementation;
+    }
+
+    function getModuleIdentifier() external view returns (uint) { return identifier; }
+    function getImplementationVersion() external view returns (uint) { return version; }
+    function getModuleName() external view returns (string memory) { return name; }
+    function getModuleDescription() external view returns (string memory) { return description; }
+}
