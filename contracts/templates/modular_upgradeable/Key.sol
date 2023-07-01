bec7d27a17aab2204214300cc78a726053863201
snqre <marco.bizxaro@gmail.com>
2023-07-01 22:54:29 +0100

    syntax fixes key and module manager should work properly

--- contracts/templates/modular_upgradeable/Key.sol
+++ contracts/templates/modular_upgradeable/Key.sol
@@ -3,31 +3,49 @@ pragma solidity ^0.8.19;
 import "contracts/templates/modular_upgradeable/ModuleManager.sol";
 
 contract Key {
-    ModuleManager private _moduleManager;
+    ModuleManager public moduleManager;
 
-    constructor(string memory nameOfKey) {
-        _moduleManager = new ModuleManager();
-        _moduleManager.aquire(nameOfKey, address(this));
+    event Connected(
+        string indexed module,
+        string indexed signature,
+        bytes indexed args,
+        uint256 version,
+        address target,
+        bytes response
+    );
+
+    constructor(string memory nameOfKey, address governor) {
+        moduleManager = new ModuleManager(address(this));
+        moduleManager.aquire(nameOfKey, address(this));
+        moduleManager.aquire("governor", governor);
     }
 
     function _connect(
         string memory module,
         string memory signature,
         bytes memory args,
-        uint version
+        uint256 version
     ) internal virtual returns (bytes memory) {
+        bool hasGovernancePermission = moduleManager.hasGovernancePermission_(
+            module
+        );
+        require(
+            hasGovernancePermission,
+            "Key: Module does not have governance permission or is not its latest implementation."
+        );
+
+        address target;
         if (version != 0) {
-            target = _moduleManager.getImplementation(module, version);
+            target = moduleManager.getImplementation(module, version);
         } else {
-            target = _moduleManager.getLatestImplementation(module);
+            target = moduleManager.getLatestImplementation(module);
         }
 
         (bool success, bytes memory response) = (target).call(
             abi.encodeWithSignature(signature, args)
         );
-        if (!success) {
-            revert FailedFunctionCall(target, signature, args, version);
-        }
+
+        require(success, "Key: Failed function call.");
 
         emit Connected(module, signature, args, version, target, response);
         return response;
@@ -37,7 +55,7 @@ contract Key {
         string memory module,
         string memory signature,
         bytes memory args,
-        uint version
+        uint256 version
     ) external returns (bytes memory) {
         return _connect(module, signature, args, version);
     }
--- contracts/templates/modular_upgradeable/ModuleManager.sol
+++ contracts/templates/modular_upgradeable/ModuleManager.sol
@@ -4,34 +4,33 @@ import "contracts/deps/openzeppelin/access/Ownable.sol";
 import "contracts/deps/openzeppelin/utils/structs/EnumerableSet.sol";
 
 interface IModuleManager {
-    function aquire(
-        string module,
-        address implementation
-    ) external returns (bool);
+    function aquire(string memory module, address implementation)
+        external
+        returns (bool);
 
-    function upgrade(
-        string module,
-        address newImplementation
-    ) external returns (bool);
+    function upgrade(string memory module, address newImplementation)
+        external
+        returns (bool);
 
-    function getLatestVersion(string module) external view returns (uint);
+    function getLatestVersion(string memory module)
+        external
+        view
+        returns (uint256);
 
-    function getLatestImplementation(
-        string module
-    ) external view returns (address);
+    function getLatestImplementation(string memory module)
+        external
+        view
+        returns (address);
 
-    function getImplementation(
-        string module,
-        uint256 version
-    ) external view returns (address);
+    function getImplementation(string memory module, uint256 version)
+        external
+        view
+        returns (address);
 
-    function hasGovernancePermission() external view returns (bool);
-
-    event ModuleAquired(string indexed module, address implementation);
-    event ModuleUpgraded(string indexed module, address newImplementation);
+    function hasGovernancePermission_() external view returns (bool);
 }
 
-contract ModuleManager is IModuleManager, Ownable {
+contract ModuleManager is Ownable {
     using EnumerableSet for EnumerableSet.AddressSet;
     uint256 public numberOfModules;
 
@@ -92,24 +91,32 @@ contract ModuleManager is IModuleManager, Ownable {
         _;
     }
 
+    event ModuleAquired(string indexed module, address implementation);
+    event ModuleUpgraded(string indexed module, address newImplementation);
+
     constructor(address owner) Ownable(owner) {}
 
-    function _getLatestVersion(
-        string memory module
-    ) internal view virtual onlyIfModuleWasFound(module) returns (uint256) {
+    function _getLatestVersion(string memory module)
+        internal
+        view
+        virtual
+        onlyIfModuleWasFound(module)
+        returns (uint256)
+    {
         return _implementations[module].length() - 1;
     }
 
-    function _getLatestImplementation(
-        string memory module
-    ) internal view virtual onlyIfModuleWasFound(module) returns (address) {
+    function _getLatestImplementation(string memory module)
+        internal
+        view
+        virtual
+        onlyIfModuleWasFound(module)
+        returns (address)
+    {
         return _implementations[module].at(_getLatestVersion(module));
     }
 
-    function _getImplementation(
-        string memory module,
-        uint256 version
-    )
+    function _getImplementation(string memory module, uint256 version)
         internal
         view
         virtual
@@ -119,10 +126,7 @@ contract ModuleManager is IModuleManager, Ownable {
         return _implementations[module].at(version);
     }
 
-    function _aquire(
-        string memory module,
-        address implementation
-    )
+    function _aquire(string memory module, address implementation)
         internal
         virtual
         onlyOwner
@@ -138,52 +142,63 @@ contract ModuleManager is IModuleManager, Ownable {
         return true;
     }
 
-    function _upgrade(
-        string memory module,
-        address newImplementation
-    ) internal virtual onlyOwner onlyIfModuleWasFound(module) returns (bool) {
+    function _upgrade(string memory module, address newImplementation)
+        internal
+        virtual
+        onlyOwner
+        onlyIfModuleWasFound(module)
+        returns (bool)
+    {
         _implementations[module].add(newImplementation);
 
         emit ModuleUpgraded(module, newImplementation);
         return true;
     }
 
-    function aquire(
-        string memory module,
-        address implementation
-    ) external returns (bool) {
+    function aquire(string memory module, address implementation)
+        external
+        returns (bool)
+    {
         return _aquire(module, implementation);
     }
 
-    function upgrade(
-        string memory module,
-        address newImplementation
-    ) external returns (bool) {
+    function upgrade(string memory module, address newImplementation)
+        external
+        returns (bool)
+    {
         return _upgrade(module, newImplementation);
     }
 
-    function getLatestVersion(
-        string memory module
-    ) external view returns (uint256) {
+    function getLatestVersion(string memory module)
+        external
+        view
+        returns (uint256)
+    {
         return _getLatestVersion(module);
     }
 
-    function getLatestImplementation(
-        string memory module
-    ) external view returns (address) {
+    function getLatestImplementation(string memory module)
+        external
+        view
+        returns (address)
+    {
         return _getLatestImplementation(module);
     }
 
-    function getImplementation(
-        string memory module,
-        uint256 version
-    ) external view returns (address) {
+    function getImplementation(string memory module, uint256 version)
+        external
+        view
+        returns (address)
+    {
         return _getImplementation(module, version);
     }
 
-    function hasGovernancePermission_(
-        string memory module
-    ) external view onlyIfModuleHasGovernancePermission(module) returns (bool) {
+    function hasGovernancePermission_(string memory module)
+        external
+        view
+        onlyIfModuleHasGovernancePermission(module)
+        returns (bool)
+    {
         return true;
     }
 }
