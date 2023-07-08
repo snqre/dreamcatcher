7d314da63f0a4dab65c48c0f98d177eaf96add89
snqre <marco.bizxaro@gmail.com>
2023-07-08 13:15:36 +0100

    Update Authenticator.sol

--- contracts/polygon/templates/modular-upgradeable/Authenticator.sol
+++ contracts/polygon/templates/modular-upgradeable/Authenticator.sol
@@ -10,21 +10,50 @@ contract Authenticator is IAuthenticator, Ownable {
 
     mapping(address => string[]) public tags;
 
-    constructor() Ownable(msg.sender) {}
+    constructor() Ownable(msg.sender) {
+        _grant(msg.sender, "can-grant-role");
+        _grant(msg.sender, "can-grant-role");
+        _grant(msg.sender, "can-grant-role");
+        _revoke(msg.sender, "can-revoke-role");
+        _revoke(msg.sender, "can-revoke-role");
+    }
 
-    function grant(address to, string memory tag)
+    function _grant(address to, string memory tag)
         public {
         tags[to].push(tag);
     }
     
-    function revoke(address from, string memory tag)
-        public {
+    function _revoke(address from, string memory tag)
+        public
+        returns (bool) {
+        bool success;
         for (uint i = 0; i < tags[from].length; i++) {
             string memory selected = tags[from][i];
             if (keccak256(abi.encodePacked(selected)) == keccak256(abi.encodePacked(tag))) {
                 tags[from][i].pop();
+                success = true;
                 break;
             }
         }
+
+        return success;
+    }
+    
+    function authenticate(address from, string memory tag)
+        public
+        returns (bool) {
+        bool success = revoke(from, tag);
+        require(success, "INSUFFICIENT_AUTHORIZATION");
+    }
+
+    function grant(address to, string memory tag)
+        public {
+        authenticate(msg.sender, "can-grant-role");
+        _grant(to, tag);
+    }
+    
+    function revoke(address from, string memory tag)
+        public {
+        authenticate(msg.sender, "can-revoke-role");
     }
 }
