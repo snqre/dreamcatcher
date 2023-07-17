ef96139dbc6e0e438c77193f914f4822b2186a57
snqre <marco.bizxaro@gmail.com>
2023-07-17 16:51:13 +0100

    updates

--- contracts/polygon/templates/modular-upgradeable/hub/Validator.sol
+++ contracts/polygon/templates/modular-upgradeable/hub/Validator.sol
@@ -4,8 +4,15 @@ import "contracts/polygon/deps/openzeppelin/utils/structs/EnumerableSet.sol";
 import "contracts/polygon/templates/modular-upgradeable/hub/__Validator.sol";
 
 interface IValidator {
-    event KeyRevoked(address indexed from, address of_, string signature);
-    event KeyGranted(address indexed to, address of_, string signature, __Validator.Class class, uint32 startTimestamp, uint32 endTimestamp, uint8 balance);
+    event KeyRevoked(address indexed from, address indexed of_, string indexed signature);
+    event KeyGranted(address indexed to, address indexed of_, string indexed signature, __Validator.Class class, uint32 startTimestamp, uint32 endTimestamp, uint8 balance);
+    event Authorized(address indexed from, address indexed of_, string indexed signature);
+
+    function revoke(address from, address of_, string memory signature) external;
+    function grant(address to, address of_, string memory signature, __Validator.Class class, uint32 startTimestamp, uint32 endTimestamp, uint8 balance) external;
+    function validate(address from, address of_, string memory signature) external;
+    function getKey(address from, address of_, string memory signature) external view returns (bytes32, __Validator.Class, uint32, uint32, uint8);
+    function getKeys(address from) external view returns (bytes32[] memory, __Validator.Class[] memory, uint32[] memory, uint32[] memory, uint8[] memory);
 }
 
 contract Validator is IValidator {
@@ -15,14 +22,13 @@ contract Validator is IValidator {
     mapping(address => mapping(bytes32 => __Validator.Data)) private _datas;
 
     function revoke(address from, address of_, string memory signature)
-        external {
+        public {
         __Validator.removeKey(_keys[from], _datas[from][__Validator.encode(of_, signature)], of_, signature);
         emit KeyRevoked(from, of_, signature);
     }
 
     function grant(address to, address of_, string memory signature, __Validator.Class class, uint32 startTimestamp, uint32 endTimestamp, uint8 balance)
-        external 
-        returns (__Validator.Key memory) {
+        public {
         require(
             !__Validator.isClass(class, __Validator.Class.DEFAULT),
             "Validator: class cannot default"
@@ -45,13 +51,34 @@ contract Validator is IValidator {
         }
         __Validator.addKey(_keys[to], _datas[to][__Validator.encode(of_, signature)], of_, signature, class, startTimestamp, endTimestamp, balance);
         emit KeyGranted(to, of_, signature, class, startTimestamp, endTimestamp, balance);
-        return __Validator.Key({
-            id: __Validator.encode(of_, signature),
-            class: class,
-            startTimestamp: startTimestamp,
-            endTimestamp: endTimestamp,
-            balance: balance
-        });
+    }
+
+    function validate(address from, address of_, string memory signature)
+        public {
+        (bytes32 id, __Validator.Class class, uint32 startTimestamp, uint32 endTimestamp, uint8 balance) = getKey(from, of_, signature);
+        require(
+            _keys[from].contains(id),
+            "Validator: key is not owned"
+        );
+        if (class == __Validator.Class.TIMED) {
+            require(
+                block.timestamp > startTimestamp,
+                "Validator: timed key has not been granted yet"
+            );
+            require(
+                block.timestamp < endTimestamp,
+                "Validator: timed key has expired"
+            );
+        }
+        else if (class == __Validator.Class.CONSUMABLE) {
+            require(
+                balance != 0,
+                "Validator: consumable key balance is zero"
+            );
+            balance--;
+        }
+        revoke(from, of_, signature);
+        grant(from, of_, signature, class, startTimestamp, endTimestamp, balance);
     }
 
     function getKey(address from, address of_, string memory signature)
@@ -61,7 +88,7 @@ contract Validator is IValidator {
     }
 
     function getKeys(address from)
-        external view
+        public view
         returns (bytes32[] memory, __Validator.Class[] memory, uint32[] memory, uint32[] memory, uint8[] memory) {
         bytes32[] memory values = _keys[from].values();
         __Validator.Class[] memory classes = new __Validator.Class[](values.length);
