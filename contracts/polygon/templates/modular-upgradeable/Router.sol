40f2bd11828abc359d12037a0293bbd87e7e317c
snqre <marco.bizxaro@gmail.com>
2023-07-17 19:33:30 +0100

    Update Router.sol

--- contracts/polygon/templates/modular-upgradeable/Router.sol
+++ contracts/polygon/templates/modular-upgradeable/Router.sol
@@ -1,7 +1,27 @@
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
-import "contracts/polygon/deps/openzeppelin/utils/structs/EnumerableSet.sol";
+import "contracts/polygon/deps/openzeppelin-upgradeable/access/OwnableUpgradeable.sol";
+import "contracts/polygon/deps/openzeppelin-upgradeable/proxy/utils/Initializable.sol";
+import "contracts/polygon/deps/openzeppelin-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract Router {
+interface IRouter {
     
+}
+
+contract Router is IRouter, Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
 }
