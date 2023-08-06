d6a5952c11ba0792dfc22f618a9c4dc8619f7943
snqre <marco.bizxaro@gmail.com>
2023-08-06 17:41:38 +0100

    Update Validator.sol

--- contracts/polygon/templates/modular-upgradeable/hubv2-eternal-storage/Validator.sol
+++ contracts/polygon/templates/modular-upgradeable/hubv2-eternal-storage/Validator.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.19;
 import "contracts/polygon/templates/Storage.sol";
-import "contracts/polygon/templates/__Encoder.sol";
 import "contracts/polygon/deps/openzeppelin/security/ReentrancyGuard.sol";
 
 contract Validator is ReentrancyGuard {
