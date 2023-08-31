2e303dd8806b8d0ecb67e5ff6ed93ff8259560a7
snqre <marco.bizxaro@gmail.com>
2023-08-31 21:40:53 +0100

    Update QuickSwapOracle.sol

--- contracts/polygon/QuickSwapOracle.sol
+++ contracts/polygon/QuickSwapOracle.sol
@@ -572,6 +572,83 @@ abstract contract Context {
 }
 
 /**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor(address initialOwner) {
+        _transferOwnership(initialOwner);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+/**
  * @dev Contract module which allows children to implement an emergency stop
  * mechanism that can be triggered by an authorized account.
  *
@@ -674,6 +751,8 @@ abstract contract Pausable is Context {
     divisor of 1,000,000 or 1,000,000,000,000,000,000 
     we need to differentiate to make sure we get the accurate price */
 contract QuickSwapOracle is Pausable {
+    
+
     IUniswapV2Factory public quickSwapFactory;
 
     modifier requirePairNotZero(address addressPair) {
@@ -767,11 +846,18 @@ contract QuickSwapOracle is Pausable {
         ) = pair.getReserves();
         uint reserve0_ = reserve0 * (10**token1.decimals());
         uint price = ((1 * reserve0_) / reserve1);
-        if (price <= 9999999999) {
-            price /= 1000000;
-        } else {
-            price /= 1000000000000000000;
-        }
         return (price, lastTimestamp);
+    }
+}
+
+contract QuickSwapOracle2 is Ownable, Pausable {
+    using EnumerableSet for EnumerableSet.AddressSet;
+    
+    EnumerableSet.AddressSet private _greenlitPairs;
+    IUniswapV2Factory public quickSwapFactory;
+
+    constructor()
+    Ownable(msg.sender) {
+        
     }
 }
