cd044c1a143831c428001b18770ce9aec55174b2
snqre <marco.bizxaro@gmail.com>
2023-09-25 19:05:50 +0100

    Update TerminalV1.sol

--- contracts/polygon/TerminalV1.sol
+++ contracts/polygon/TerminalV1.sol
@@ -15,229 +15,243 @@ import "contracts/polygon/ProxyStateOwnableContract.sol";
 * address       -> "map", <string/name>     -> <address/proxy>
 * addressSet    -> "proxies", "deployed"    -> <addressSet/proxies>
 * addressSet    -> "proxies", "supported"   -> <addressSet/proxies>
-* addressSet    -> "proxies", "paused"      -> <addressSet/proxies>
-*       
+* addressSet    -> "history", <string/name> -> <addressSet/implementations>
  */
-
-
-
-
-
-
-/**
-* version 0.5.0
-*
-* @dev A component is like a part that works in the protocol. Components 
-*      often talk to other components, making the protocol interactions 
-*      complex and hard to follow. The Terminal is in charge of improving 
-*      our proxies. It helps the protocol handle its owned proxies in an 
-*      organized way. It displays different states like supported, 
-*      unsupported, paused, unpaused, and more. It also enables multiple 
-*      upgrades to happen in a single transaction.
-*
-* _address:         "map", <string/name>        -> <address/component>
-* _addressSet:      "components", "deployed"    -> <addressSet/components>
-*
-* 
- */
-contract TerminalV1B is ProxyStateOwnableContract {
+contract TerminalV1 is ProxyStateOwnableContract {
 
     /** Events. */
 
     /**
-    * @dev Fired when a new proxy is deployed.
+    * @dev Emitted when TERMINAL deploys a new proxy.
      */
     event ProxyDeployed(string indexed name, address indexed proxy);
-
+    
     /**
-    * @dev Fired when a proxy is upgraded.
+    * @dev Emitted when TERMINAL upgrades a supported proxy.
+    *
+    * WARNING: Upgrade pauses and unpauses proxies but the respective
+    *          events are not emitted because the state returns to
+    *          normal.
      */
     event ProxyUpgraded(string indexed name, address indexed proxy, address indexed implementation);
 
     /**
-    * @dev Fired when a proxy is paused.
+    * @dev Emitted when TERMINAL pauses a supported proxy.
      */
     event ProxyPaused(string indexed name, address indexed proxy);
 
     /**
-    * @dev Fired when a proxy is unpaused.
+    * @dev Emitted when TERMINAL unpauses a supported proxy.
      */
     event ProxyUnpaused(string indexed name, address indexed proxy);
 
     /**
-    * @dev Fired when a proxy is let go off. Unsupported proxies cannot
-    *      be called and do not have owners, this means they are
-    *      immutable and cannot be changed afterwards. users should
-    *      not expect anymore upgrades for that proxy.
+    * @dev Emitted when TERMINAL releases a supported proxy.
+    *      Turns a supported proxy into an unsupported proxy which
+    *      cannot be manipulated by the TERMINAL.
      */
-    event ProxyUnsupported(string indexed name, address indexed proxy);
-
-    /**
-    * @dev Fired when a proxy is imported typically an alternative way
-    *      of assigning a new proxy to a name.
-    *
-    * WARNING: Imported proxies will be called using the same interface
-    *          as deployed proxies.
-     */
-    event ProxyImported(string indexed name, address indexed proxy);
+    event ProxyReleased(string indexed name, address indexed proxy);
 
     /** External. */
 
     /**
-    * @dev Deploy proxy.
+    * @dev Deploys a proxy contract and assigns a name to it so it is
+    *      more human readable. Proxies are searchable within the
+    *      contract and can be refered to with names rather than
+    *      addresses which are harder to read. Proxies are upgradeable
+    *      and new functionality can be given to them. The idea is for
+    *      the TERMINAL to be managed by the governor contract so that
+    *      the community is directly responsible for granting
+    *      permission to change functionality.
      */
-    function deployProxy(string calldata name) external onlyOwner() whenNotPaused() returns (address) {
-        return _deployProxy(name);
+    function deploy(string calldata name) external onlyOwner() whenNotPaused() returns (address) {
+        
+        /**
+        * @dev Require name is not assigned to a proxy address. Assign
+        *      name to deployed proxy address.
+         */
+        require(_address[keccak256(abi.encode("map", name))] == address(0), "TerminalV1: name is already assigned");
+        _address[keccak256(abi.encode("map", name))] = new ProxyStateOwnableContract();
+        
+        /**
+        * @dev Add newly deployed proxy to deployed proxies set.
+        *
+        * WARNING: We don't check if the address is already in the set
+        *          because we don't expect the address to point to an
+        *          existing contract when deployed.
+         */
+        _addressSet[keccak256(abi.encode("proxies", "deployed"))].add(_address[keccak256(abi.encode("map", name))]);
+        
+        /**
+        * @dev Add newly deployed proxy to supported proxies set.
+        *
+        * NOTE: Only supported proxies are able to be upgraded, paused,
+        *       unpaused, or mutated by TERMINAL. Released proxies are
+        *       immutable as the Terminal renounces its ownership of
+        *       them.
+         */
+        _addressSet[keccak256(abi.encode("proxies", "supported"))].add(_address[keccak256(abi.encode("map", name))]);
+
+        /**
+        * @dev Initialize the newly deployed proxy contract to gain
+        *      ownership of it.
+        *
+        * WARNING: If the initialize call is front run, this function
+        *          SHOULD revert. The reason initialize must be called
+        *          is because when using proxies contructors alter
+        *          contracts on a byte code level. Therefore, they
+        *          have to be disabled, and initialize must be called.
+         */
+        IProxyStateOwnable proxyInterface = IProxyStateOwnable(_address[keccak256(abi.encode("map", name))]);
+
+        /**
+        * @dev Function execution within ProxyStateOwnable for 
+        *      initialize().
+        *
+        * bytes32 location = keccak256(abi.encode("$initialized"));
+        * require(!_bool[location], "ProxyStateOwnable: already initialized");
+        * _transferOwnership(_msgSender());
+        * location = keccak256(abi.encode("$paused"));
+        * _bool[location] = false;
+        * location = keccak256(abi.encode("$initialized"));
+        * _bool[location] = true;
+        * emit Initialized(_msgSender());
+         */
+        proxyInterface.initialize();
+
+        emit ProxyDeployed(name, _address[keccak256(abi.encode("map", name))]);
+
+        return address(proxyInterface);
     }
 
     /**
-    * @dev Import proxy.
+    * @dev Upgrades a proxy with new functionality. Will pause, before
+    *      the upgrade, and then unpause after the upgrade. Refer to
+    *      the proxy by name.
      */
-    function importProxy(string calldata name, address proxy) external onlyOwner() whenNotPaused() {
-        _importProxy(name, proxy);
+    function upgradeTo(string calldata name, address implementation) external onlyOwner() whenNotPaused() {
+
+        /**
+        * @dev Require name is assigned to a proxy address.
+         */
+        require(_address[keccak256(abi.encode("map", name))] != address(0), "TerminalV1: name is unassigned");
+
+        /**
+        * @dev Require that the proxy is supported.
+         */
+        require(_addressSet[keccak256(abi.encode("proxies", "supported"))].contains(_address[keccak256(abi.encode("map", name))]), "TerminalV1: proxy is unsupported");
+        
+        IProxyStateOwnable proxyInterface = IProxyStateOwnable(_address[keccak256(abi.encode("map", name))]);
+
+        /**
+        * @dev If proxy is unpaused when upgraded the proxy is paused
+        *      and once it is upgraded it returns to the unpaused
+        *      state.
+         */
+        if (!proxyInterface.paused()) {
+
+            proxyInterface.pause();
+
+            proxyInterface.upgrade(implementation);
+
+            proxyInterface.unpause();
+        }
+
+        /**
+        * @dev If proxy is paused when upgraded the proxy remains paused
+        *      during the upgrade and stays in the state it was.
+         */
+        else {
+
+            proxyInterface.upgrade(implementation);
+        }
+        
+
+        /**
+        * @dev Add new implementation to the proxy's implementation
+        *      history.
+        *
+        * WARNING: addressSet will not add duplicate entries. If the upgrade
+        *          is to an older implementation, the new implementation
+        *          will not be added as a new one. If it is an older
+        *          version that has been implemented it can be viewed
+        *          within the addressSet.
+         */
+        _addressSet[keccak256(abi.encode("history", name))].add(implementation);
+        
+        emit ProxyUpgraded(name, _address[keccak256(abi.encode("map", name))], implementation);
     }
 
     /**
-    * @dev Upgrade proxy.
+    * @dev Pauses a supported proxy.
      */
-    function upgradeProxy(string calldata name, address implementation) external onlyOwner() whenNotPaused() {
-        _upgradeProxy(name, implementation);
+    function pause_(string calldata name) external onlyOwner() whenNotPaused() {
+
+        /**
+        * @dev Require name is assigned to a proxy address.
+         */
+        require(_address[keccak256(abi.encode("map", name))] != address(0), "TerminalV1: name is unassigned");
+
+        /**
+        * @dev Require that the proxy is supported.
+         */
+        require(_addressSet[keccak256(abi.encode("proxies", "supported"))].contains(_address[keccak256(abi.encode("map", name))]), "TerminalV1: proxy is unsupported");
+
+        IProxyStateOwnable proxyInterface = IProxyStateOwnable(_address[keccak256(abi.encode("map", name))]);
+
+        proxyInterface.pause();
+
+        emit ProxyPaused(name, _address[keccak256(abi.encode("map", name))]);
     }
 
     /**
-    * @dev Pause proxy.
+    * @dev Unpauses a supported proxy.
      */
-    function pauseProxy(string calldata name) external onlyOwner() whenNotPaused() {
-        _pauseProxy(name);
+    function unpause_(string calldata name) external onlyOwner() whenNotPaused() {
+
+        /**
+        * @dev Require name is assigned to a proxy address.
+         */
+        require(_address[keccak256(abi.encode("map", name))] != address(0), "TerminalV1: name is unassigned");
+
+        /**
+        * @dev Require that the proxy is supported.
+         */
+        require(_addressSet[keccak256(abi.encode("proxies", "supported"))].contains(_address[keccak256(abi.encode("map", name))]), "TerminalV1: proxy is unsupported");
+
+        IProxyStateOwnable proxyInterface = IProxyStateOwnable(_address[keccak256(abi.encode("map", name))]);
+
+        proxyInterface.unpause();
+
+        emit ProxyUnpaused(name, _address[keccak256(abi.encode("map", name))]);
     }
 
     /**
-    * @dev Unpause proxy.
+    * @dev Release a supported proxy so it will not be supported anymore
+    *      and TERMINAL will not have ownership anymore.
      */
-    function unpauseProxy(string calldata name) external onlyOwner() whenNotPaused() {
-        _unpauseProxy(name);
-    }
+    function release(string calldata name) external onlyOwner() whenNotPaused() {
 
-    /**
-    * @dev Unsupport proxy.
-     */
-    function unsupportProxy(string calldata name) external onlyOwner() whenNotPaused() {
-        _unsupportProxy(name);
-    }
+        /**
+        * @dev Require name is assigned to a proxy address.
+         */
+        require(_address[keccak256(abi.encode("map", name))] != address(0), "TerminalV1: name is unassigned");
 
-    /** Internal View. */
+        /**
+        * @dev Require that the proxy is supported.
+         */
+        require(_addressSet[keccak256(abi.encode("proxies", "supported"))].contains(_address[keccak256(abi.encode("map", name))]), "TerminalV1: proxy is unsupported");
 
-    /**
-    * @dev Get name to proxy mapping. Name will return the address of the
-    *      deployed or imported proxy. If the name has not been assigned
-    *      will return address(0).
-     */
-    function _getMap(string calldata name) internal view returns (address) {
-        return _address[keccak256(abi.encode(name, "mapping"))];
-    }
+        IProxyStateOwnable proxyInterface = IProxyStateOwnable(_address[keccak256(abi.encode("map", name))]);
 
-    /** Internal. */
-    
-    /**
-    * @dev Set name to proxy mapping. Assign a proxy address to a name of
-    *      a deployed or imported proxy.
-     */
-    function _setMap(string calldata name, address proxy) internal {
-        _address[keccak256(abi.encode(name, "mapping"))] = proxy;
-    }
+        proxyInterface.renounceOwnership();
 
-    /**
-    * @dev Deploy a new proxy.
-     */
-    function _deployProxy(string calldata name) internal returns (address) {
-        /** Require that the name is not assigned to a proxy address. */
-        require(_getMap(name) == address(0), "TerminalV1: assigned");
-        _setMap(name, new ProxyStateOwnableContract());
-        IProxyStateOwnable proxy = IProxyStateOwnable(_getMap(name));
-        proxy.initialize();
-        /** Append proxy in deployed array. */
-        EnumerableSet.AddressSet storage proxies = _addressSet[keccak256(abi.encode("components", "deployed"))];
-        proxies.add(address(proxy));
-        /** Return. */
-        emit ProxyDeployed(name, address(proxy));
-        return address(proxy);
-    }
+        /**
+        * @dev Remove the released proxy from the addressSet of supported
+        *      proxies.
+         */
+        _addressSet[keccak256(abi.encode("proxies", "supported"))].remove(_address[keccak256(abi.encode("map", name))]);
 
-    /**
-    * @dev Import and assign a new proxy.
-     */
-    function _importProxy(string calldata name, address proxy) internal {
-        /** Require that the name is not assigned to a proxy address. */
-        require(_getMap(name) == address(0), "TerminalV1: assigned");
-        _setMap(name, proxy);
-        /** Here we assume the proxy is already initialized when imported. */
-        /** Append proxy in deployed array. */
-        EnumerableSet.AddressSet storage proxies = _addressSet[keccak256(abi.encode("components", "deployed"))];
-        proxies.add(proxy);
-        /** Return. */
-        emit ProxyImported(name, proxy);
-    }
-
-    /**
-    * @dev Upgrade a proxy.
-     */
-    function _upgradeProxy(string calldata name, address implementation) internal {
-        /** Require that the name is assigned to a proxy address. */
-        require(_getMap(name) != address(0), "TerminalV1: unassigned");
-        IProxyStateOwnable proxy = IProxyStateOwnable(_getMap(name));
-        proxy.pause();
-        proxy.upgrade(implementation);
-        proxy.unpause();
-        /** 
-        * Append new implementation to proxy upgrade history. Duplicate
-        * entries are not added but remain in the position it was in.
-        * The upgrade will still occur but it will point to an older
-        * version within the proxy history addressSet.
-        */
-        EnumerableSet.AddressSet storage proxyHistory = _addressSet[keccak256(abi.encode(name, "history"))];
-        proxyHistory.add(implementation);
-        emit ProxyUpgraded(name, address(proxy), implementation);
-    }
-
-    /**
-    * @dev Pause proxy.
-     */
-    function _pauseProxy(string calldata name) internal {
-        /** Require that the name is assigned to a proxy address. */
-        require(_getMap(name) != address(0), "TerminalV1: unassigned");
-        IProxyStateOwnable proxy = IProxyStateOwnable(_getMap(name));
-        proxy.pause();
-        /** Append to paused proxy array. */
-        EnumerableSet.AddressSet storage pausedProxies = _addressSet[keccak256(abi.encode("paused"))];
-        pausedProxies.add(address(proxy));
-        emit ProxyPaused(name, address(proxy));
-    }
-
-    /**
-    * @dev Unpause proxy.
-     */
-    function _unpauseProxy(string calldata name) internal {
-        /** Require that the name is assigned to a proxy address. */
-        require(_getMap(name) != address(0), "TerminalV1: unassigned");
-        IProxyStateOwnable proxy = IProxyStateOwnable(_getMap(name));
-        proxy.unpause();
-        /** Remove from paused proxy array. */
-        EnumerableSet.AddressSet storage pausedProxies = _addressSet[keccak256(abi.encode("paused"))];
-        pausedProxies.remove(address(proxy));
-        emit ProxyUnpaused(name, address(proxy));
-    }
-
-    /**
-    * @dev Unsupport a proxy and renounce ownership.
-     */
-    function _unsupportProxy(string calldata name) internal {
-        /** Require that the name is assigned to a proxy address. */
-        require(_getMap(name) != address(0), "TerminalV1: unassigned");
-        IProxyStateOwnable proxy = IProxyStateOwnable(_getMap(name));
-        proxy.renounceOwnership();
-        /** Append to unsupported proxy array. */
-        EnumerableSet.AddressSet storage unsupportedProxies = _addressSet[keccak256(abi.encode("unsupported"))];
-        unsupportedProxies.add(address(proxy));
-        emit ProxyUnsupported(name, address(proxy));
+        emit ProxyReleased(name, _address[keccak256(abi.encode("map", name))]);
     }
 }
