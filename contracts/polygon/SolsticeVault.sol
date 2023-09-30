051188ab455c5a9f5fc18fc02c7bed8f2a5a478f
snqre <marco.bizxaro@gmail.com>
2023-09-30 02:35:37 +0100

    Update SolsticeVault.sol

--- contracts/polygon/SolsticeVault.sol
+++ contracts/polygon/SolsticeVault.sol
@@ -3,7 +3,31 @@ pragma solidity 0.8.19;
 
 import "contracts/polygon/libraries/__Finance.sol";
 
+import "contracts/polygon/external/openzeppelin/utils/structs/EnumerableSet.sol";
+
+/**
+* => Depsit => Management => Withdrawal
+*
+*
+ */
 contract SolsticeVault {
 
-    
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    uint256 public minDeposit;
+    uint256 public maxDeposit;
+
+    uint256 public minWithdrawal;
+    uint256 public maxWithdrawal;
+
+    bool public live;
+
+    EnumerableSet.AddressSet private _allowedIn;
+
+    function _onlyLive() internal view {
+
+        require(live, "!live");
+    }
+
+
 }
