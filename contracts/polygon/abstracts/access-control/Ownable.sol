41ec667f2e65592b36d97c2593c556afd79ff4a4
snqre <marco.bizxaro@gmail.com>
2023-11-06 19:26:34 +0000

    Update Ownable.sol

--- contracts/polygon/abstracts/access-control/Ownable.sol
+++ contracts/polygon/abstracts/access-control/Ownable.sol
@@ -2,4 +2,18 @@
 pragma solidity 0.8.19;
 import "contracts/polygon/abstracts/storage/StorageLite.sol";
 
-abstract contract Ownable is 
+abstract contract Ownable is StorageLite {
+    address internal _owner;
+
+    event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
+
+    function owner() external view virtual returns (address) {
+        return _owner;
+    }
+
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
