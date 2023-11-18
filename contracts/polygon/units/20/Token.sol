14582009d7a675663d27039e8fce705f3974885f
snqre <marco.bizxaro@gmail.com>
2023-11-18 01:15:31 +0000

    Update

--- contracts/polygon/units/20/Token.sol
+++ contracts/polygon/units/20/Token.sol
@@ -6,14 +6,19 @@ import 'contracts/polygon/deps/openzeppelin/token/ERC20/extensions/ERC20Snapshot
 import 'contracts/polygon/deps/openzeppelin/token/ERC20/extensions/ERC20Permit.sol';
 import 'contracts/polygon/deps/openzeppelin/token/ERC20/extensions/IERC20Metadata.sol';
 import 'contracts/polygon/deps/openzeppelin/token/ERC20/extensions/IERC20Permit.sol';
+import 'contracts/polygon/deps/openzeppelin/access/Ownable.sol';
 
-contract Token is ERC20, ERC20Burnable, ERC20Snapshot, ERC20Permit {
-    constructor(string memory name, string memory symbol, uint initSupply) ERC20(name, symbol) ERC20Permit(name) {
+contract Token is ERC20, ERC20Burnable, ERC20Snapshot, ERC20Permit, Ownable {
+    constructor(string memory name, string memory symbol, uint initSupply) ERC20(name, symbol) ERC20Permit(name) Ownable(msg.sender) {
         _mint(msg.sender, initSupply * (10**18));
     }
 
     function getCurrentSnapshotId() external view virtual returns (uint) {
         return _getCurrentSnapshotId();
+    }
+
+    function mint(address account, uint amount) external virtual onlyOwner {
+        _mint(account, amount);
     }
 
     function snapshot() external virtual returns (uint) {
