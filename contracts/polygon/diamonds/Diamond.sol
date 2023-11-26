3ebd23c82ee55560483b2e0336c1c432d986f87b
snqre <marco.bizxaro@gmail.com>
2023-11-26 21:01:06 +0000

    Clean up

--- contracts/polygon/diamonds/Diamond.sol
+++ contracts/polygon/diamonds/Diamond.sol
@@ -5,7 +5,4 @@ import "contracts/polygon/abstracts/ERC2535.sol";
 /**
 * ref https://eips.ethereum.org/EIPS/eip-2535#abstract
  */
-contract Diamond is ERC2535 {
-
-
-}
+contract Diamond is ERC2535 {}
