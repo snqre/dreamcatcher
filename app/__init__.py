8c066eb96531bc31ea21d69ae329e2a7293faa57
snqre <marco.bizxaro@gmail.com>
2023-12-07 16:28:43 +0000

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from json import *
 from typing import *
 from flask import *
+from json_sync import *
+
+settings:JsonSync = JsonSync('app/static/json/settings.json')
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("base.html")
+    return render_template([
+        'base.html',
+        settings.pageName
+    ])
 
 if __name__ == "__main__":
     app.run(debug=True)
