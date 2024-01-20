86e40727ca2c345a17bd581ef7d73c91524289bf
snqre <marco.bizxaro@gmail.com>
2024-01-20 21:47:39 +0000

    Init cargo new app interface **app

---
+++ interface/target/debug/deps/interface-2360b6ccce659959.d
@@ -0,0 +1,5 @@
+C:\Users\snqre\Documents\GitHub\dreamcatcher\interface\target\debug\deps\libinterface-2360b6ccce659959.rmeta: src\main.rs
+
+C:\Users\snqre\Documents\GitHub\dreamcatcher\interface\target\debug\deps\interface-2360b6ccce659959.d: src\main.rs
+
+src\main.rs:
---
+++ interface/target/debug/deps/interface.d
@@ -0,0 +1,5 @@
+C:\Users\snqre\Documents\GitHub\dreamcatcher\interface\target\debug\deps\interface.exe: src\main.rs
+
+C:\Users\snqre\Documents\GitHub\dreamcatcher\interface\target\debug\deps\interface.d: src\main.rs
+
+src\main.rs:
